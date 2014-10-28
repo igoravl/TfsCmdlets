d0fac60bec07d1b0efccaf82bb620c018e9833dd
Igor Abade <igoravl@hotmail.com>
2014-10-28 19:11:03 -0200

    Adds cmdlet Get-TfsWorkItemTypeDefinition

--- TShooter.TfsCmdlets/TfsCmdlets.psm1
+++ TShooter.TfsCmdlets/TfsCmdlets.psm1
@@ -419,6 +419,51 @@ function Export-TfsGlobalLists
 	}
 }
 
+#===================================
+# Work Item Type cmdlets
+#===================================
+
+Function Get-TfsWorkItemTypeDefinition
+{
+	param
+	(
+		[Parameter(Mandatory=$true)] [string] 
+		$CollectionUrl,
+    
+		[Parameter(Mandatory=$true)] [string] 
+		$ProjectName,
+    
+		[Parameter(ValueFromPipeline=$true)] [string] 
+		$Name,
+
+		[switch] 
+		$UseDefaultCredentials,
+    
+		[switch] 
+		$IncludeGlobalLists,
+    
+		[Parameter()] [ValidateNotNull()] [System.Management.Automation.Credential()] [System.Management.Automation.PSCredential]
+		$Credential = [System.Management.Automation.PSCredential]::Empty
+	)
+
+	process
+	{
+		$tpc = Get-TfsTeamProjectCollection -CollectionUrl $CollectionUrl -UseDefaultCredentials:$UseDefaultCredentials.IsPresent -Credential $Credential
+
+		$store = $tpc.GetService([type]'Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItemStore')
+
+		$project = $store.Projects[$ProjectName]
+
+		foreach($witd in $project.WorkItemTypes)
+		{ 
+			if (($Name -eq "") -or ($witd.Name -eq $Name))
+			{
+				 return $witd
+			}
+		}
+	}
+}
+
 #===========================
 # Connection cmdlets
 #===========================
