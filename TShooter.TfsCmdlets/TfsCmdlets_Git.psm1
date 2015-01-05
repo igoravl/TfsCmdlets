086091b46c84f101eda14b74fea1b880d9b69219
Igor Abade V. Leite <igoravl@hotmail.com>
2015-01-05 11:22:06 -0200

    Removes obsolete ad-hoc connection parameters

--- TShooter.TfsCmdlets/TfsCmdlets_Git.psm1
+++ TShooter.TfsCmdlets/TfsCmdlets_Git.psm1
@@ -6,34 +6,23 @@ Function New-GitRepository
 {
     param
     (
-        [Parameter(ParameterSetName="Not connected",Mandatory=$true)]
-        [string] 
-        $CollectionUrl = $null,
-    
-        [Parameter(ParameterSetName="Not connected",Mandatory=$true)]
-        [Parameter(ParameterSetName="Connected",Mandatory=$true)]
+        [Parameter(Mandatory=$true)]
         [string] 
         $ProjectName,
     
-        [Parameter(ParameterSetName="Not connected",Mandatory=$true, ValueFromPipeline=$true)]
-        [Parameter(ParameterSetName="Connected",Mandatory=$true, ValueFromPipeline=$true)]
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
         [string] 
-        $Name,
-    
-        [Parameter(ParameterSetName="Not connected")]
-        [switch] 
-        $UseDefaultCredentials,
-    
-        [Parameter(ParameterSetName="Not connected")]
-        [System.Management.Automation.PSCredential] [System.Management.Automation.Credential()]
-        $Credential = [System.Management.Automation.PSCredential]::Empty
+        $Name
     )
+
+	Begin
+	{
+        $connection = _GetRestConnection
+	}
 
     Process
     {
-        $connection = _GetRestConnection  -CollectionUrl $CollectionUrl -UseDefaultCredentials:$UseDefaultCredentials.IsPresent -Credential $Credential -ParameterSetName $PSCmdlet.ParameterSetName
-        $project = Get-TeamProjectInformation -CollectionUrl $connection.Url -Name $ProjectName -UseDefaultCredentials:$connection.UseDefaultCredentials -Credential $connection.Credential
-
+        $project = Get-TeamProjectInformation -Name $ProjectName
         $id = $project.id
         $apiUrl = _GetUrl $CollectionUrl "_apis/git/repositories?api-version=1.0"
         $body = "{`"name`": `"${Name}`", `"project`": { `"id`": `"${id}`" } }"
