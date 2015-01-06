7a1e54506f9e50aee47ecb8f6f3e46abf34092c9
Igor Abade V. Leite <igoravl@hotmail.com>
2015-01-06 00:04:46 -0200

    Removes redundant files

--- TShooter.TfsCmdlets/TfsCmdlets-Test.ps1
+++
@@ -1,3 +0,0 @@
-Import-Module D:\Projects\VSO_Lambda3\ALM\TfsCmdlets\Src\Lambda3.TfsCmdlets\Lambda3.TfsCmdlets\TfsCmdlets.psm1 -Force 
-
-New-TfsGitRepository -CollectionUrl "http://localhost:8080/tfs/DefaultCollection/" -ProjectName "GitProject" -Name "NewRepo" -UseDefaultCredentials
