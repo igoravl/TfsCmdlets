8381e3bcdf7f6af1fa00cee9a9554ef5f42982e5
Igor Abade V. Leite <igoravl@hotmail.com>
2015-01-08 02:52:04 -0200

    Adds duplicate detection logic to Add-GlobalListItem

--- TShooter.TfsCmdlets.UnitTests/GlobalList.Tests.ps1
+++ TShooter.TfsCmdlets.UnitTests/GlobalList.Tests.ps1
@@ -2,77 +2,130 @@ Get-Content "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\_TestSetup.ps1" 
 
 InModuleScope TfsCmdlets {
 
-	Describe "New-GlobalList" {
-		Mock Import-GlobalLists { } -ModuleName GlobalList
-		Mock Export-GlobalLists {
-		return [xml]'<GLOBALLISTS>
+    Describe "New-GlobalList" {
+        Mock Import-GlobalLists { } -ModuleName GlobalList
+        Mock Export-GlobalLists {
+        return [xml]'<GLOBALLISTS>
 <GLOBALLIST name="List1">
-	<LISTITEM value="List item 1" />
+    <LISTITEM value="Item 1" />
 </GLOBALLIST>
 <GLOBALLIST name="List2">
-	<LISTITEM value="List item 1" />
+    <LISTITEM value="Item 1" />
 </GLOBALLIST>
 </GLOBALLISTS>'
-	} -ModuleName GlobalList
+    } -ModuleName GlobalList
 
-		Context "When the list does not exist" {
+        Context "When the list does not exist" {
 
-			It "Creates a new list" {
-				$result = New-GlobalList -Name "New List" -Items "Item 1"
-				$result.Name | Should Be "New List"
+            It "Creates a new list" {
+                $result = New-GlobalList -Name "New List" -Items "Item 1"
+                $result.Name | Should Be "New List"
                 $result.Items.Length | Should Be 1
-                $result.Overwritten | Should Be $false
-			}
-		}
+                $result.IsOverwritten | Should Be $false
+            }
 
-		Context "When the list exists" {
+            It "Creates a new list with many items" {
+                $result = New-GlobalList -Name "New List" -Items "Item 1", "Item 2"
 
-			It "Creates a new list with -Force" {
-				$result = New-GlobalList -Name "List1" -Items "Item 1" -Force
-				$result.Name | Should Be "List1"
+                $result.Name | Should Be "New List"
+                $result.Items.Length | Should Be 2
+                $result.Items[0] | Should Be "Item 1"
+                $result.Items[1] | Should Be "Item 2"
+                $result.IsOverwritten | Should Be $false
+            }
+       }
+
+        Context "When the list exists" {
+
+            It "Creates a new list with -Force" {
+                $result = New-GlobalList -Name "List1" -Items "Item 1" -Force
+                $result.Name | Should Be "List1"
                 $result.Items.Length | Should Be 1
-                $result.Overwritten | Should Be $true
-			}
+                $result.IsOverwritten | Should Be $true
+            }
 
-			It "Throws without -Force" {
-				{ New-GlobalList -Name "List1" -Items "Item 1" } | Should Throw
-			}
-		}
-	}
+            It "Creates a new list with many items with -Force" {
+                $result = New-GlobalList -Name "List1" -Items "Item 1", "Item 2" -Force
 
-	Describe "Add-GlobalListItem" {
-		Mock Import-GlobalLists { } -ModuleName GlobalList
-		Mock Export-GlobalLists {
-		return [xml]'<GLOBALLISTS>
+                $result.Name | Should Be "List1"
+                $result.Items.Length | Should Be 2
+                $result.Items[0] | Should Be "Item 1"
+                $result.Items[1] | Should Be "Item 2"
+                $result.IsOverwritten | Should Be $true
+            }
+
+            It "Throws without -Force" {
+                { New-GlobalList -Name "List1" -Items "Item 1" } | Should Throw
+            }
+        }
+    }
+
+    Describe "Add-GlobalListItem" {
+        Mock Import-GlobalLists { } -ModuleName GlobalList
+        Mock Export-GlobalLists {
+        return [xml]'<GLOBALLISTS>
 <GLOBALLIST name="List1">
-	<LISTITEM value="List item 1" />
+    <LISTITEM value="Item 1" />
 </GLOBALLIST>
 <GLOBALLIST name="List2">
-	<LISTITEM value="List item 1" />
+    <LISTITEM value="Item 1" />
 </GLOBALLIST>
 </GLOBALLISTS>'
-	} -ModuleName GlobalList
+    } -ModuleName GlobalList
 
-		Context "When the list doesn't exist" {
+        Context "When the list doesn't exist" {
 
-			It "Throws without -Force" {
-				{ Add-GlobalListItem -Name "New List" -Item "Item 1" } | Should Throw
-			}
+            It "Creates the list and the item with -Force" {
+                $result = Add-GlobalListItem -Name "New List" -Items "Item 1" -Force
 
-			It "Creates the list and the item with -Force" {
-				$result = Add-GlobalListItem -Name "New List" -Items "Item 1" -Force
+                $result.Name | Should Be "New List"
+                $result.Items.Length | Should Be 1
+                $result.Items[0] | Should Be "Item 1"
+                $result.IsNewList | Should Be $true
+            }
 
-				$attrValue = $result.SelectSingleNode("//GLOBALLIST[@name='New List']/@name")
-				$attrValue.Value | Should Be "New List"
+            It "Throws without -Force" {
+                { Add-GlobalListItem -Name "New List" -Item "Item 1" } | Should Throw
+            }
+        }
 
-				$attrValue = $result.SelectSingleNode("//GLOBALLIST[@name='New List']/LISTITEM/@value")
-				$attrValue.Value | Should Be "Item 1"
-			}
+        Context "When the list exists" {
+        	
+            It "Adds a new item" {
+                $result = Add-GlobalListItem -Name "List1" -Items "Item 2"
 
-		}
+                $result.Name | Should Be "List1"
+                $result.Items.Length | Should Be 2
+                $result.Items[1] | Should Be "Item 2"
+            }
 
-		#Context "When the list exists" {
-		#	It 
-		#}
-	}
+            It "Adds many items" {
+                $result = Add-GlobalListItem -Name "List1" -Items "Item 2", "Item 3"
+
+                $result.Name | Should Be "List1"
+                $result.Items.Length | Should Be 3
+                $result.Items[0] | Should Be "Item 1"
+                $result.Items[1] | Should Be "Item 2"
+                $result.Items[2] | Should Be "Item 3"
+            }
+
+            It "Ignores an existing item" {
+                $result = Add-GlobalListItem -Name "List1" -Items "Item 1"
+
+                $result.Name | Should Be "List1"
+                $result.Items.Length | Should Be 1
+                $result.Items[0] | Should Be "Item 1"
+                $result.IsNewList | Should Be $false
+            }
+
+            It "Adds many items but ignores an existing item" {
+                $result = Add-GlobalListItem -Name "List1" -Items "Item 1", "Item 2"
+
+                $result.Name | Should Be "List1"
+                $result.Items.Length | Should Be 2
+                $result.Items[0] | Should Be "Item 1"
+                $result.Items[1] | Should Be "Item 2"
+            }
+        }
+    }
 }
