da2ddeceddf1da494d2141e4e8f564f60217e48d
Igor Abade V. Leite <igoravl@hotmail.com>
2015-03-14 12:18:14 -0300

    Add preliminary support to Team extended data

--- TShooter.TfsCmdlets/Functions/Team.psm1
+++ TShooter.TfsCmdlets/Functions/Team.psm1
@@ -4,32 +4,237 @@
 
 Function New-TfsTeam
 {
-	param
-	(
-		[Parameter(Mandatory=$true)]
-		[string] 
-		$ProjectName,
-	
-		[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-		[string] 
-		$Name,
-	
-		[Parameter()]
-		[string] 
-		$Description
-	)
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] 
+        $ProjectName,
+    
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string] 
+        $Name,
+    
+        [Parameter()]
+        [string] 
+        $Description,
+    
+        [Parameter()]
+        [string] 
+        $Picture,
+    
+        [Parameter()]
+        [System.DayOfWeek[]] 
+        $WorkingDays,
+    
+        [Parameter()]
+        [switch] 
+        $ShowBugsInBacklog,
+    
+        [Parameter()]
+        [switch] 
+        $Default
+    )
 
-	Process
-	{
-		$tpc = Get-TfsTeamProjectCollection -Current
+    Process
+    {
+        $tpc = Get-TfsTeamProjectCollection -Current
 
-		# Get Team Project
-		$cssService = $tpc.GetService([type]"Microsoft.TeamFoundation.Server.ICommonStructureService3")
-		$teamProject = $cssService.GetProjectFromName($ProjectName)
+        $teamProject = _GetTeamProject $ProjectName
+        $team = _CreateTeam $teamProject $Name $Description
 
-		# Create Team
-		$teamService = $tpc.GetService([type]"Microsoft.TeamFoundation.Client.TfsTeamService")
+        
 
-		$teamService.CreateTeam($teamProject.Uri, $Name, $Description, $null)
-	}
+    }
 }
+
+Function Get-TfsTeam
+{
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] 
+        $ProjectName,
+    
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string] 
+        $Name
+    )
+
+    Process
+    {
+        $team = _GetTeam $ProjectName $Name
+
+        return [PSCustomObject] [ordered] @{
+            Name = $team.Name;
+            Description = $team.Description;
+            Picture = "(Not Implemented)";
+            WorkingDays = "(Not Implemented)";
+            TeamFieldValues = "(Not Implemented)";
+            ShowBugsInBacklog = "(Not Implemented)";
+            Default = "(Not Implemented)";
+            Id = $team.Identity.TeamFoundationId
+        }
+    }
+}
+
+Function Set-TfsTeam
+{
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] 
+        $ProjectName,
+    
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string] 
+        $Name,
+    
+        [Parameter()]
+        [string] 
+        $NewName,
+    
+        [Parameter()]
+        [string] 
+        $Description,
+    
+        [Parameter()]
+        [string] 
+        $Picture,
+    
+        [Parameter()]
+        [System.DayOfWeek[]] 
+        $WorkingDays,
+    
+        [Parameter()]
+        [switch] 
+        $ShowBugsInBacklog
+    )
+
+    Process
+    {
+        $team = _GetTeam -ProjectName $ProjectName -Name $Name
+
+        if ($NewName)
+        {
+        }
+    }
+}
+
+Function Remove-TfsTeam
+{
+}
+
+Function Add-TfsTeamFieldValue
+{
+    
+}
+
+Function Set-TfsTeamFieldValue
+{
+
+}
+
+Function Remove-TfsTeamFieldValue
+{
+    
+}
+
+# =================
+# Helper Functions
+# =================
+
+Function _GetTeamProject
+{
+    param
+    (
+        [Parameter()]
+        [string] 
+        $ProjectName
+    )
+
+    Process
+    {
+        $cssService = $tpc.GetService([type]"Microsoft.TeamFoundation.Server.ICommonStructureService3")
+        return $cssService.GetProjectFromName($projectName)
+    }
+}
+
+Function _GetTeam
+{
+    param
+    (
+        [Parameter()]
+        [string] 
+        $ProjectName,
+    
+        [Parameter()]
+        [string] 
+        $Name
+    )
+
+    Begin
+    {
+        $tpc = Get-TfsTeamProjectCollection -Current
+        $teamService = $tpc.GetService([type]"Microsoft.TeamFoundation.Client.TfsTeamService")
+    }
+
+    Process
+    {
+        $teamProject = _GetTeamProject $projectName
+        $team = ($teamService.QueryTeams($teamProject.Uri) | where Name -eq $name)
+
+        return $team
+    }
+}
+
+Function _CreateTeam
+{
+    param
+    (
+    )
+
+    Process
+    {
+        $teamService = $tpc.GetService([type]"Microsoft.TeamFoundation.Client.TfsTeamService")
+        return $teamService.CreateTeam($teamProject.Uri, $Name, $Description, $null)
+    }
+}
+
+Function _SetTeamSettings
+{
+    param
+    (
+        $team, 
+        
+        [string[]]
+        $teamFieldValues        
+    )
+}
+
+function Get-TfsTeamSettingsConfigurationService {
+    Param(
+        [Microsoft.TeamFoundation.Client.TfsTeamProjectCollection] $TfsCollection
+        )
+    return $TfsCollection.GetService([ Microsoft.TeamFoundation.ProcessConfiguration.Client.TeamSettingsConfigurationService]);
+} 
+ 
+function Add-TfsTeamField {
+Param(
+       [parameter(Mandatory=$true)][Microsoft.TeamFoundation.Client.TfsTeamProjectCollection] $TfsCollection,
+       [parameter(Mandatory=$true)][Microsoft.TeamFoundation.Server.ProjectInfo] $TfsTeamProject,
+       [parameter(Mandatory=$true)][Microsoft.TeamFoundation.Client.TeamFoundationTeam] $TfsTeam,
+       [parameter(Mandatory=$true)][String] $TeamFieldValue
+       )
+ 
+    $TfsTeamConfigService = Get-TfsTeamSettingsConfigurationService $TfsCollection
+    $TfsTeamConfig = $TfsTeamConfigService.GetTeamConfigurations([Guid[]]($TfsTeam.Identity.TeamFoundationId))
+ 
+    $newTeamFieldValue = New-Object Microsoft.TeamFoundation.ProcessConfiguration.Client.TeamFieldValue
+    $newTeamFieldValue.Value = $TeamFieldValue
+ 
+    $TfsTeamConfig.TeamSettings.TeamFieldValues = [Microsoft.TeamFoundation.ProcessConfiguration.Client.TeamFieldValue[]]($newTeamFieldValue)
+    $TfsTeamConfig.TeamSettings.BacklogIterationPath = "$($TfsTeamProject.Name)" 
+    #$TfsTeamConfig.TeamSettings.IterationPaths = [string[]]("$($TfsTeamProject.Name)")
+ 
+    $TfsTeamConfigService.SetTeamSettings($TfsTeam.Identity.TeamFoundationId,$TfsTeamConfig.TeamSettings)
+} 
