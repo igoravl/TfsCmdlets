6129f8816a28b1d920ff19d0a044203704ee367c
Igor Abade V. Leite <igoravl@gmail.com>
2015-06-10 18:50:24 -0300

    Implement WITD import and export

--- TShooter.TfsCmdlets/Functions/WorkItem.psm1
+++ TShooter.TfsCmdlets/Functions/WorkItem.psm1
@@ -1,6 +1,7 @@
 Function Get-TfsWorkItemType
 {
 	[CmdletBinding()]
+	[OutputType([Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItemType[]])]
 	Param
 	(
 		[Parameter()]
@@ -11,29 +12,88 @@ Function Get-TfsWorkItemType
 		[object]
 		[ValidateNotNull()]
 		[ValidateScript({($_ -is [string]) -or ($_ -is [Microsoft.TeamFoundation.WorkItemTracking.Client.Project])})] 
-		$Project
+		$Project,
+
+		[Parameter()]
+        [object]
+        $Collection
 	)
 
 	Process
 	{
-		$tp = _GetTeamProject $Project
+		$tp = _GetTeamProject $Project $Collection
 		return $tp.WorkItemTypes | ? Name -Like $Name
 	}
 }
 
 Function Import-TfsWorkItemType
 {
+	[CmdletBinding()]
+	[OutputType([Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItemType[]])]
+	Param
+	(
+		[Parameter()]
+		[xml] 
+		$Xml,
 
+		[Parameter(ValueFromPipeline=$true, Mandatory=$true)]
+		[object]
+		[ValidateNotNull()]
+		[ValidateScript({($_ -is [string]) -or ($_ -is [Microsoft.TeamFoundation.WorkItemTracking.Client.Project])})] 
+		$Project,
+
+		[Parameter()]
+        [object]
+        $Collection
+	)
+
+	Process
+	{
+		$tp = _GetTeamProject $Project $Collection
+		return $tp.WorkItemTypes | ? Name -Like $Name
+	}
 }
 
 Function Export-TfsWorkItemType
 {
+	[CmdletBinding()]
+	[OutputType([xml[]])]
+	Param
+	(
+		[Parameter()]
+		[string] 
+		$Name = "*",
 
+		[Parameter()]
+		[switch]
+		$IncludeGlobalLists,
+
+		[Parameter(ValueFromPipeline=$true, Mandatory=$true)]
+		[object]
+		[ValidateNotNull()]
+		[ValidateScript({($_ -is [string]) -or ($_ -is [Microsoft.TeamFoundation.WorkItemTracking.Client.Project])})] 
+		$Project,
+
+		[Parameter()]
+        [object]
+        $Collection
+	)
+
+	Process
+	{
+		$types = Get-TfsWorkItemType -Name $Name -Project $Project -Collection $Collection
+
+		foreach($type in $types)
+		{
+			$type.Export($IncludeGlobalLists)
+		}
+	}
 }
 
 Function New-TfsWorkItem
 {
 	[CmdletBinding()]
+	[OutputType([Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItem])]
 	Param
 	(
 		[Parameter(ValueFromPipeline=$true, Mandatory=$true)]
@@ -52,12 +112,16 @@ Function New-TfsWorkItem
 		[Parameter()]
 		[object]
 		[ValidateScript({($_ -eq $null) -or ($_ -is [string]) -or ($_ -is [Microsoft.TeamFoundation.WorkItemTracking.Client.Project])})] 
-		$Project
+		$Project,
+
+		[Parameter()]
+        [object]
+        $Collection
 	)
 
 	Process
 	{
-		$wit = _GetWorkItemType $Type $Project
+		$wit = _GetWorkItemType $Type $Project $Collection
 
 		$wi = $wit.NewWorkItem()
 		$wi.Title = $Title
@@ -75,26 +139,26 @@ Function New-TfsWorkItem
 
 Function _GetTeamProject
 {
-	Param ($Project)
+	Param ($Project, $Collection)
 
-	if ($Project -is [string])
+	if ($Project -is [Microsoft.TeamFoundation.WorkItemTracking.Client.Project])
 	{
-		return Get-TfsTeamProject -Name $Project
+		return $Project
 	}
 
-	return $Project
+	return Get-TfsTeamProject -Name $Project -Collection $Collection
 }
 
 Function _GetWorkItemType
 {
-	Param ($Type, $Project)
+	Param ($Type, $Project, $Collection)
 
 	if ($Type -is [Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItemType])
 	{
 		return $Type
 	}
 
-	$tp = _GetTeamProject $Project
+	$tp = _GetTeamProject $Project $Collection
 
 	return $tp.WorkItemTypes[$Type]
 }
