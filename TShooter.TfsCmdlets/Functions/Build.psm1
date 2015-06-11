6554931352e49ff2eeefae2fb1d3e2a0818bfee8
Vinicius Moura <vinijmoura@hotmail.com>
2015-06-11 17:15:06 -0300

    Implement Helper to Start-TfsBuild and Get-TfsBuildQueue

--- TShooter.TfsCmdlets/Functions/Build.psm1
+++ TShooter.TfsCmdlets/Functions/Build.psm1
@@ -1,5 +1,5 @@
 <#
-.Synopsis
+.SYNOPSIS
    Sets up an additional build service in the current computer
 .DESCRIPTION
    In Team Foundation Server, the Build Service is a Windows Service that is associated with a particular TFS Team Project Collection (since 2010). 
@@ -85,6 +85,26 @@ Function New-TfsBuildService
     }
 }
 
+<#
+.SYNOPSIS
+	Queues a new Build.
+
+.PARAMETER BuildDefinition
+	Build Definition Name that you want to queue.
+
+.PARAMETER Collection
+	Specifies either a URL or the name of the Team Project Collection to connect to, or a previously initialized TfsTeamProjectCollection object.
+	For more details, see the -Collection argument in the Get-TfsTeamProjectCollection cmdlet.
+
+.PARAMETER Project
+	Specifies either the name of the Team Project or a previously initialized Microsoft.TeamFoundation.WorkItemTracking.Client.Project object to connect to. 
+	For more details, see the -Project argument in the Get-TfsTeamProject cmdlet. 
+
+.EXAMPLE
+	Start-TfsBuild -BuildDefinition "My Build Definition" -Project "MyTeamProject"
+	This example queue a Build Definition "My Build Definition" of Team Project "MyTeamProject".
+
+#>
 Function Start-TfsBuild
 {
 	Param
@@ -154,7 +174,31 @@ Function Start-TfsBuild
         $buildServer.QueueBuild($req)
 	}
 }
+<#
+.SYNOPSIS
+Get all queued builds
 
+.PARAMETER BuildDefinition
+	Uses this parameter to filter for an specific Build Defintion.
+	If suppress, cmdlet will de shown all queue builds.
+
+.PARAMETER Collection
+	Specifies either a URL or the name of the Team Project Collection to connect to, or a previously initialized TfsTeamProjectCollection object.
+	For more details, see the -Collection argument in the Get-TfsTeamProjectCollection cmdlet.
+
+.PARAMETER Project
+	Specifies either the name of the Team Project or a previously initialized Microsoft.TeamFoundation.WorkItemTracking.Client.Project object to connect to. 
+	For more details, see the -Project argument in the Get-TfsTeamProject cmdlet. 
+
+.EXAMPLE
+Get-TfsBuildQueue -BuildDefinition "My Build Definition" -Project "My Team Project"
+Get all queued builds given a definition name and a team project name
+
+.EXAMPLE
+Get-TfsBuildQueue
+Get all queued builds, regardless of definition name or team project name
+
+#>
 Function Get-TfsBuildQueue
 {
 	Param
