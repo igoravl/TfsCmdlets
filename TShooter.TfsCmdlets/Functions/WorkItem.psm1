bd83da8efbf2fe0a1f480f6ccf527301e54ad4a6
Vinicius Moura <vinijmoura@hotmail.com>
2015-06-12 14:03:35 -0300

    Implement Helper to Import-TfsWorkItemType, Export-TfsWorkItemType, New-TfsWorkItem

--- TShooter.TfsCmdlets/Functions/WorkItem.psm1
+++ TShooter.TfsCmdlets/Functions/WorkItem.psm1
@@ -50,6 +50,28 @@ Function Get-TfsWorkItemType
 	}
 }
 
+<#
+.SYNOPSIS
+	This cmdlet imports a work item type XML definition file into a team project on a Team Foundation Server.
+	If a work item type with the same name already exists, the new work item type definition will overwrite the existing definition.
+	If the work item type does not already exist, a new work item type will be created..
+
+.PARAMETER Xml
+	Specifies the work item type XML definition file to import.
+
+.PARAMETER Collection
+	Specifies either a URL or the name of the Team Project Collection to connect to, or a previously initialized TfsTeamProjectCollection object.
+	For more details, see the -Collection argument in the Get-TfsTeamProjectCollection cmdlet.
+
+.PARAMETER Project
+	Specifies either the name of the Team Project or a previously initialized Microsoft.TeamFoundation.WorkItemTracking.Client.Project object to connect to. 
+	For more details, see the -Project argument in the Get-TfsTeamProject cmdlet. 
+
+.EXAMPLE
+	Import-TfsWorkItemType -Xml "C:\temp\Task.xml" -Project "MyTeamProject"
+	This example imports a Work Item Type Task on Team Project "MyTeamProject".
+#>
+
 Function Import-TfsWorkItemType
 {
 	[CmdletBinding()]
@@ -77,6 +99,30 @@ Function Import-TfsWorkItemType
 		return $tp.WorkItemTypes | ? Name -Like $Name
 	}
 }
+
+<#
+.SYNOPSIS
+	Exports a work item type on XML format.
+
+.PARAMETER Name
+	Uses this parameter to filter for an specific Work Item Type.
+	If suppress, cmdlet will return all Work Item Types on XML format.
+
+.PARAMETER IncludeGlobalLists
+	 Exports the definitions of referenced global lists. If not specified, global list definitions are omitted.
+
+.PARAMETER Collection
+	Specifies either a URL or the name of the Team Project Collection to connect to, or a previously initialized TfsTeamProjectCollection object.
+	For more details, see the -Collection argument in the Get-TfsTeamProjectCollection cmdlet.
+
+.PARAMETER Project
+	Specifies either the name of the Team Project or a previously initialized Microsoft.TeamFoundation.WorkItemTracking.Client.Project object to connect to. 
+	For more details, see the -Project argument in the Get-TfsTeamProject cmdlet. 
+
+.EXAMPLE
+	$xml = Export-TfsWorkItemType -Name Task -Project "MyTeamProject" -IncludeGlobalLists
+	This example export a Work Item Type Task of Team Project "MyTeamProject" including the respective GlobalLists.
+#>
 
 Function Export-TfsWorkItemType
 {
@@ -113,6 +159,35 @@ Function Export-TfsWorkItemType
 		}
 	}
 }
+
+<#
+.SYNOPSIS
+	Specifies that a new work item of the type entered be created. The object WorkItem of the new work item is displayed.
+
+.PARAMETER Type
+	Represents the name of the work item type to create.
+
+.PARAMETER Title
+	Specifies a Title field of new work item type that will be created.
+
+.PARAMETER Fields
+	Specifies the fields that are changed and the new values to give to them.
+	FieldN The name of a field to update.
+	ValueN The value to set on the fieldN.
+	[field1=value1[;field2=value2;...]
+
+.PARAMETER Collection
+	Specifies either a URL or the name of the Team Project Collection to connect to, or a previously initialized TfsTeamProjectCollection object.
+	For more details, see the -Collection argument in the Get-TfsTeamProjectCollection cmdlet.
+
+.PARAMETER Project
+	Specifies either the name of the Team Project or a previously initialized Microsoft.TeamFoundation.WorkItemTracking.Client.Project object to connect to. 
+	For more details, see the -Project argument in the Get-TfsTeamProject cmdlet. 
+
+.EXAMPLE
+	New-TfsWorkItem -Type Task -Title "Task 1" -Project "MyTeamProject"
+	This example creates a new Work Item on Team Project "MyTeamProject".
+#>
 
 Function New-TfsWorkItem
 {
