3e47a3d089abf68b65bbb511b4f8f8ba0b819ea0
Igor Abade <igoravl@gmail.com>
2016-12-24 00:48:27 -0200

    Fix test setup logic

--- src/Tests/_TestSetup.ps1
+++ src/Tests/_TestSetup.ps1
@@ -1,10 +1,12 @@
-$projectDir = Join-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) '..\TfsCmdlets' -Resolve
-$modulePath = Join-Path $projectDir 'bin\Release\TfsCmdlets\TfsCmdlets.psd1'
+$outputDir = Join-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) '..\out' -Resolve
+$modulePath = Join-Path $projectDir 'Module\TfsCmdlets.psd1'
 $hasBuild = Test-Path $modulePath
 
-if (-not $hasBuild) {
+if (-not $hasBuild)
+{
     throw "Module TfsCmdlets not found at $modulePath. Build project TfsCmdlets.Build prior to running tests."
 }
 
 Get-Module TfsCmdlets | Remove-Module
-Import-Module $modulePath 
+
+Import-Module $modulePath -Force
