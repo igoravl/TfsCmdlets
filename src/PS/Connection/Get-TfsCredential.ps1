2d994e199cc8e4d6c78b4535a61722ee105dc981
Igor Abade <igoravl@gmail.com>
2017-03-29 00:55:08 -0300

    Add verbose messages to Get-TfsCredential

--- src/PS/Connection/Get-TfsCredential.ps1
+++ src/PS/Connection/Get-TfsCredential.ps1
@@ -46,6 +46,7 @@ Function Get-TfsCredential
         
         if (($parameterSetName -eq 'Credential object') -and (-not $Credential))
         {
+            Write-Verbose "Parameter Credential was specified but no value was provided. Defaulting to cached credentials"
             $parameterSetName = 'Cached Credential'
         }
 
@@ -54,11 +55,13 @@ Function Get-TfsCredential
         switch($parameterSetName)
         {
             'Cached Credential' {
+                Write-Verbose "Returning cached credentials"
                 $fedCred = New-Object 'Microsoft.TeamFoundation.Client.CookieCredential' -ArgumentList $true
                 $winCred = New-Object 'Microsoft.TeamFoundation.Client.WindowsCredential' -ArgumentList $true
             }
 
             'User name and password' {
+                Write-Verbose "Returning user name / password credentials from arguments UserName and Password"
                 $netCred = New-Object 'System.Net.NetworkCredential' -ArgumentList $UserName, $Password
                 $fedCred = New-Object 'Microsoft.TeamFoundation.Client.BasicAuthCredential' -ArgumentList $netCred
                 $winCred = New-Object 'Microsoft.TeamFoundation.Client.WindowsCredential' -ArgumentList $netCred
@@ -67,15 +70,18 @@ Function Get-TfsCredential
             'Credential object' {
                 if ($Credential -is [Microsoft.TeamFoundation.Client.TfsClientCredentials])
                 {
+                    Write-Verbose "Credentials argument is of type TfsClientCredentials. Returning that object unmodified."
                     return $Credential
                 }
 
                 if($Credential -is [pscredential])
                 {
+                    Write-Verbose "Credentials argument is of type PSCredential. Deriving a new user name/password credential from it."
                     $netCred = $Credential.GetNetworkCredential()
                 }
                 elseif ($Credential -is [System.Net.NetworkCredential])
                 {
+                    Write-Verbose "Credentials argument is of type NetworkCredential. Deriving a new user name/password credential from it."
                     $netCred = $Credential
                 }
                 else
@@ -88,12 +94,14 @@ Function Get-TfsCredential
             }
 
             'Personal Access Token' {
+                Write-Verbose "Returning PAT-based credentials from PersonalAccessToken argument."
                 $netCred = New-Object 'System.Net.NetworkCredential' -ArgumentList 'dummy-pat-user', $PersonalAccessToken
                 $fedCred = New-Object 'Microsoft.TeamFoundation.Client.BasicAuthCredential' -ArgumentList $netCred
                 $winCred = New-Object 'Microsoft.TeamFoundation.Client.WindowsCredential' -ArgumentList $netCred
             }
 
             'Prompt for credential' {
+                Write-Verbose "Returning an empty, interactive login credential."
                 $fedCred = New-Object 'Microsoft.TeamFoundation.Client.CookieCredential' -ArgumentList $false
                 $winCred = New-Object 'Microsoft.TeamFoundation.Client.WindowsCredential' -ArgumentList $false
                 $allowInteractive = $true
