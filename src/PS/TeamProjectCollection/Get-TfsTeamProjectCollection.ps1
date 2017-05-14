3a77d3049a177badfc1fd2e33e238e2e3b1af298
Igor Abade <igoravl@gmail.com>
2017-05-14 01:22:47 -0300

    Fix missing documentation

--- src/PS/TeamProjectCollection/Get-TfsTeamProjectCollection.ps1
+++ src/PS/TeamProjectCollection/Get-TfsTeamProjectCollection.ps1
@@ -1,28 +1,33 @@
 <#
 .SYNOPSIS
-    Gets information about one or more team project collections.
+Gets information about one or more team project collections.
 
 .DESCRIPTION
-	The Get-TfsTeamProjectCollection cmdlets gets one or more Team Project Collection objects (an instance of Microsoft.TeamFoundation.Client.TfsTeamProjectCollection) from a TFS instance. 
-	Team Project Collection objects can either be obtained by providing a fully-qualified URL to the collection or by collection name (in which case a TFS Configuration Server object is required).
+The Get-TfsTeamProjectCollection cmdlets gets one or more Team Project Collection objects (an instance of Microsoft.TeamFoundation.Client.TfsTeamProjectCollection) from a TFS instance. 
+Team Project Collection objects can either be obtained by providing a fully-qualified URL to the collection or by collection name (in which case a TFS Configuration Server object is required).
 
 .PARAMETER Collection
-	${HelpParam_Collection}
+${HelpParam_Collection}
 
 .PARAMETER Server
-	Specifies either a URL/name of the Team Foundation Server configuration server (the "root" of a TFS installation) to connect to, or a previously initialized Microsoft.TeamFoundation.Client.TfsConfigurationServer object.
+Specifies either a URL/name of the Team Foundation Server configuration server (the "root" of a TFS installation) to connect to, or a previously initialized Microsoft.TeamFoundation.Client.TfsConfigurationServer object.
+
+.PARAMETER Current
+Returns the team project collection specified in the last call to Connect-TfsTeamProjectCollection (i.e. the "current" project collection)
 
 .PARAMETER Credential
-    ${HelpParam_TfsCredential}
+${HelpParam_TfsCredential}
 
 .EXAMPLE
-	Get-TfsTeamProjectCollection http://
+Get-TfsTeamProjectCollection http://
 
 .INPUTS
-	Microsoft.TeamFoundation.Client.TfsConfigurationServer
+Microsoft.TeamFoundation.Client.TfsConfigurationServer
+System.String
+System.Uri
 
 .NOTES
-	Cmdlets in the TfsCmdlets module that operate on a collection level require a TfsConfigurationServer object to be provided via the -Server argument. If absent, it will default to the connection opened by Connect-TfsConfigurationServer.
+Cmdlets in the TfsCmdlets module that operate on a collection level require a TfsConfigurationServer object to be provided via the -Server argument. If absent, it will default to the connection opened by Connect-TfsConfigurationServer.
 #>
 Function Get-TfsTeamProjectCollection
 {
