2427c8b8903a4e802d6e583d7a534a12042101ee
Igor Abade <igoravl@gmail.com>
2017-05-14 01:23:07 -0300

    Add verb/noun tests

--- src/Tests/Correctness.Tests.ps1
+++ src/Tests/Correctness.Tests.ps1
@@ -10,6 +10,7 @@ $valueReturningVerbs = "Get|$passthruVerbs"
 $cmdletBindingRegexExpr = '\[CmdletBinding.+\]'
 $cmdletBindingRegex = [regex] $cmdletBindingRegexExpr
 
+$verbs = (Get-Verb | Select -ExpandProperty Verb -Unique | Sort)
 
     Describe 'Correctness Tests' {
 
@@ -19,6 +20,14 @@ $cmdletBindingRegex = [regex] $cmdletBindingRegexExpr
 
                 $cmd = $_
                 $cmdletBindingDefinition = $cmdletBindingRegex.Match($cmd.Definition).Value
+
+                It 'Uses an approved verb' {
+                    $verbs.Contains($cmd.Verb) | Should Be $true
+                }
+
+                It 'Uses uses the standard prefix' {
+                    $cmd.Noun.Substring(0, 3) | Should Be 'Tfs'
+                }
 
                 It 'Has [CmdletBinding()] annotation' {
                     $cmdletBindingDefinition | Should Match $cmdletBindingRegexExpr
@@ -54,6 +63,38 @@ $cmdletBindingRegex = [regex] $cmdletBindingRegexExpr
                         $cmd.Definition -match 'if\s? \(\$Passthru\)'
                     }
                 }
+
+                $cmdDocs = Get-Help $cmd.Name
+
+                It 'Has minimal documentation (Synopsis, description, examples)' {
+                    $missingSections = @()
+                    if (-not $cmdDocs.Synopsis) { $missingSections += 'Synopsis' }
+                    if (-not $cmdDocs.Description) { $missingSections += 'Description' }
+                    if (-not $cmdDocs.Examples) { $missingSections += 'Examples' }
+                    ($missingSections -join ', ') | Should BeNullOrEmpty
+                }
+
+                It 'Pipeline parameters and input types are properly set' {
+                    $pipelineBoundParam = ($cmd.Parameters.Values | ? {$_.Attributes | ? { $_.ValueFromPipeline -eq $true }})
+                    $inputDocs = $cmdDocs.inputTypes
+                    if ($pipelineBoundParam -or $hasInputDocs)
+                    {
+                        $pipelineBoundParam | Should Not BeNullOrEmpty
+                        $inputDocs | Should Not BeNullOrEmpty
+                    }
+                }
+
+                $parameterDocs = $cmdDocs.Parameters.parameter
+
+                It "All parameters have a description" {
+                    $paramsWithoutDesc = ($parameterDocs | Where-Object description -eq $null | Select-Object -ExpandProperty Name) -join ', '
+                    $paramsWithoutDesc | Should BeNullOrEmpty
+                }
+
+                # if (($pdoc.defaultvalue -match '\*') )
+                # {
+                #     It " - Parameter $pName has a default value containing '*' and [SupportsWildcards()] attribute"
+                # }
             }
         }
     }
