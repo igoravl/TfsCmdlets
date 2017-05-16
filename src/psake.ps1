72a417235faabb208c0eba16c073b1b1449ec322
Igor Abade <igoravl@gmail.com>
2017-05-16 07:57:50 -0700

    Remove -SkipPublisherCheck argument from Install-Module

--- src/psake.ps1
+++ src/psake.ps1
@@ -148,7 +148,7 @@ Task Test -Depends Build {
 
     if (-not (Get-Module PSScriptAnalyzer -ListAvailable))
     {
-        Install-Module PSScriptAnalyzer -Scope CurrentUser -Force -SkipPublisherCheck
+        Install-Module PSScriptAnalyzer -Scope CurrentUser -Force
     }
 
     $quiet = ($VerbosePreference -ne 'Continue')
