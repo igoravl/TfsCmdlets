4b52021c3a482d5fb815b69500a9996cd975351f
Igor Abade <igoravl@gmail.com>
2017-09-21 09:10:03 -0700

    Add initial service hook support

---
+++ src/PS/ServiceHook/Get-TfsServiceHookConsumer.ps1
@@ -0,0 +1,26 @@
+Function Get-TfsServiceHookConsumer
+{
+    [Cmdletbinding()]
+    [OutputType([Microsoft.VisualStudio.Services.ServiceHooks.WebApi.Consumer])]
+    Param
+    (
+        [Parameter(Position=0)]
+        [SupportsWildcards()]
+        [Alias('Name')]
+        [Alias('Id')]
+        [string]
+        $Consumer = '*',
+
+        [Parameter()]
+        [object]
+        $Collection
+    )
+
+    Process
+    {
+        $tpc = Get-TfsTeamProjectCollection -Collection $Collection
+        $client = Get-TfsHttpClient -Type 'Microsoft.VisualStudio.Services.ServiceHooks.WebApi.ServiceHooksPublisherHttpClient' -Collection $tpc
+
+        $client.GetConsumersAsync().Result | Where-Object {($_Name -Like $Consumer) -or ($_.Id -Like $Consumer)}
+    }
+}
---
+++ src/PS/ServiceHook/Get-TfsServiceHookSubscription.ps1
@@ -0,0 +1,23 @@
+Function Get-TfsServiceHookSubscription
+{
+    [Cmdletbinding()]
+    [OutputType([Microsoft.VisualStudio.Services.ServiceHooks.WebApi.Subscription])]
+    Param
+    (
+        [Parameter(Position=0)]
+        [string]
+        $Name = '*',
+
+        [Parameter()]
+        [object]
+        $Collection
+    )
+
+    Process
+    {
+        $tpc = Get-TfsTeamProjectCollection -Collection $Collection
+        $client = Get-TfsHttpClient -Type 'Microsoft.VisualStudio.Services.ServiceHooks.WebApi.ServiceHooksPublisherHttpClient' -Collection $tpc
+
+        $client.QuerySubscriptionsAsync().Result
+    }
+}
