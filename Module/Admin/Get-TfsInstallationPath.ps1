15e1616fcd71e212bd663368428fcb18b2a976c1
Igor Abade (MSA) <igoravl@hotmail.com>
2019-06-26 00:22:37 -0300

    Move utility function to separate file

--- Module/Admin/Get-TfsInstallationPath.ps1
+++ Module/Admin/Get-TfsInstallationPath.ps1
@@ -61,30 +61,3 @@ Function Get-TfsInstallationPath
 		return Invoke-ScriptBlock -ScriptBlock $scriptBlock -Computer $Computer -Credential $Credential -ArgumentList $Version, $Component
 	}
 }
-
-
-Function _GetInstallationPath($Version, $Component)
-{
-	$rootKeyPath = "HKLM:\\Software\\Microsoft\\TeamFoundationServer\\$Version"
-
-	if ($Component -eq 'BaseInstallation')
-	{
-		$componentPath = $rootKeyPath
-	}
-	else
-	{
-		$componentPath = "$rootKeyPath\\InstalledComponents\\$Component"
-	}
-
-	if (-not (Test-RegistryValue -Path $rootKeyPath -Value 'InstallPath'))
-	{
-		throw "Team Foundation Server is not installed in computer $env:COMPUTERNAME"
-	}
-
-	if (-not (Test-RegistryValue -Path $componentPath -Value 'InstallPath'))
-	{
-		throw "Team Foundation Server component '$Component' is not installed in computer $env:COMPUTERNAME"
-	}
-
-	return Get-RegistryValue -Path $componentPath -Value 'InstallPath'
-}
