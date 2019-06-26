abcdb34a58acd1eba116f6887946f6675cff0a61
Igor Abade (MSA) <igoravl@hotmail.com>
2019-06-26 04:02:40 -0300

    Extract RegisterAssemblyResolver

--- Module/Startup.ps1
+++ Module/Startup.ps1
@@ -1,14 +1,6 @@
 # Initialize Shell
 
-if ($TfsCmdletsDebugStartup)
-{
-    # For some reason, setting VerbosePreference here breaks the script. Using Set-Alias to work around it
-    
-    Write-Host "Entering TfsCmdlets startup debug mode" -ForegroundColor DarkYellow
-    Set-Alias Write-Verbose Write-Host -Option Private
-}
-
-if ($Host.UI.RawUI.WindowTitle -match "(Azure DevOps Shell*)|(Azure DevOps Shell*)")
+if ($Host.UI.RawUI.WindowTitle -match "(Azure DevOps Shell*)|(Team Foundation Server Shell*)")
 {
     # SetConsoleColors
     $Host.UI.RawUI.BackgroundColor = "DarkMagenta"
@@ -22,36 +14,4 @@ if ($Host.UI.RawUI.WindowTitle -match "(Azure DevOps Shell*)|(Azure DevOps Shell
     Write-Host ""
 
     . (Join-Path $PSScriptRoot 'Prompt.ps1')
-}
-
-# Configure assembly resolver
-
-Write-Verbose 'Registering custom Assembly Resolver'
-
-if (-not [type]::GetType('TfsCmdlets.AssemblyResolver'))
-{
-    Write-Verbose "Compiling $PSEdition version of the assembly resolver"
-
-    $sourcePath = (Join-Path $PSScriptRoot "_cs/$($PSEdition)AssemblyResolver.cs")
-    $sourceText = (Get-Content $sourcePath -Raw)
-
-    Add-Type -TypeDefinition $sourceText -Language CSharp
-
-    $libPath = (Join-Path $PSScriptRoot 'Lib')
-    $assemblies = [System.Collections.Generic.Dictionary[string,string]]::new()
-
-    Write-Verbose "Enumerating assemblies from $libPath"
-
-    foreach($f in (Get-ChildItem $libPath -Filter '*.dll'))
-    {
-        Write-Verbose "Adding $f to list of private assemblies"
-        $assemblies.Add($f.BaseName, $f.FullName)
-    }
-
-    Write-Verbose 'Calling AssemblyResolver.Register()'
-    [TfsCmdlets.AssemblyResolver]::Register($assemblies, [bool]($TfsCmdletsDebugStartup))
-}
-else
-{
-    Write-Verbose 'Custom Assembly Resolver already registered; skipping'
 }
--- Module/TfsCmdlets.psm1
+++ Module/TfsCmdlets.psm1
@@ -3,4 +3,7 @@
 $script:IsDesktop = ($PSEdition -ne 'Core')
 $script:IsCore = -not $script:IsDesktop
 
+# Configure assembly resolver
+
+_RegisterAssemblyResolver
 _ImportRequiredAssembly 'Microsoft.TeamFoundation.Client'
