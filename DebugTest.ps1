826f153ce3b3503cd561878964589a0e5ae35a17
Igor Abade (MSA) <igoravl@hotmail.com>
2019-06-28 17:00:48 -0300

    Adjust settings to simplify debugging in Code

--- DebugTest.ps1
+++
@@ -1,7 +0,0 @@
-$scriptPath = Split-Path $MyInvocation.MyCommand.Path -Parent
-
-Get-Module TfsCmdlets | Remove-Module
-
-Import-Module (Join-Path $scriptPath 'out\module\TfsCmdlets.psd1')
-
-Get-Module TfsCmdlets
