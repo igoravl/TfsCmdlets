dd8d128b9a9e5ddbfd5134a2c0be803c1723f536
Igor Abade (MSA) <igoravl@hotmail.com>
2019-07-02 20:39:48 -0300

    Add new cmdlet

---
+++ Module/Helpers/Get-TfsRestClient.ps1
@@ -0,0 +1,20 @@
+Function Get-TfsRestClient
+{
+    [CmdletBinding()]
+    [OutputType('Microsoft.VisualStudio.Services.WebApi.VssHttpClientBase')]
+    Param
+    (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]
+        $Type,
+
+        [Parameter()]
+        [object] 
+        $Collection
+    )
+
+    Process
+    {
+        return _GetRestClient @PSBoundParameters
+    }
+}
