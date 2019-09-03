f9990b62e45f7f9e0c51fe179461ca5555285a4d
Igor Abade <igoravl@gmail.com>
2019-09-03 02:35:11 -0300

    Update shell color and prompt logic

--- Module/Startup.ps1
+++ Module/Startup.ps1
@@ -1,17 +1,19 @@
 # Initialize Shell
 
-if ($Host.UI.RawUI.WindowTitle -match "(Azure DevOps Shell*)|(Team Foundation Server Shell*)")
+if (-not ($Host.UI.RawUI.WindowTitle -match "(Azure DevOps Shell*)|(Team Foundation Server Shell*)"))
 {
-    # SetConsoleColors
-    $Host.UI.RawUI.BackgroundColor = "DarkMagenta"
-    $Host.UI.RawUI.ForegroundColor = "White"
-    Clear-Host
-
-    # ShowBanner
-    $module = Test-ModuleManifest -Path (Join-Path $PSScriptRoot 'TfsCmdlets.psd1')
-    Write-Host "TfsCmdlets: $($module.Description)"
-    Write-Host "Version $($module.PrivateData.Build)"
-    Write-Host ""
-
-    . (Join-Path $PSScriptRoot 'Prompt.ps1')
+    return
 }
+
+Clear-Host
+
+# Show banner
+
+$module = Test-ModuleManifest -Path (Join-Path $PSScriptRoot 'TfsCmdlets.psd1')
+Write-Host "TfsCmdlets: $($module.Description)"
+Write-Host "Version $($module.PrivateData.Build)"
+Write-Host ""
+
+# Laad prompt
+
+. (Join-Path $PSScriptRoot 'Prompt.ps1')
