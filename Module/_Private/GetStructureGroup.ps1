8a11f0dd34fff104a50cdf9d7d4b1f6f9aa018ed
Igor Abade <igoravl@gmail.com>
2019-09-06 00:51:36 -0300

    Consolidate area/iteration cmdlets

--- Module/_Private/GetStructureGroup.ps1
+++ Module/_Private/GetStructureGroup.ps1
@@ -1,5 +1,7 @@
 Function _GetStructureGroup($StructureGroup)
 {
+    Write-Warning $MyInvocation.InvocationName
+
     if($StructureGroup -is [Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.TreeStructureGroup])
     {
         _Log "StructureGroup already set to '$StructureGroup'; returning"
@@ -11,6 +13,8 @@ Function _GetStructureGroup($StructureGroup)
     
     foreach($frame in Get-PSCallStack)
     {
+        _Log "Command '$($frame.Command)'"
+
         if ($frame.Command.EndsWith('Area'))
         {
             return [Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.TreeStructureGroup]::Areas
