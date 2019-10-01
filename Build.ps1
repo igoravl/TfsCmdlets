eb82f47380b0ba65e8ee5fa1fd655458618918d8
Igor Abade <igoravl@gmail.com>
2019-10-01 16:49:48 -0300

    Add PlatyPS to build-time modules

--- Build.ps1
+++ Build.ps1
@@ -18,7 +18,7 @@ Param
 Function Install-Dependencies
 {
     $NugetPackages = @('GitVersion.CommandLine')
-    $PsModules = @('psake', 'PsScriptAnalyzer', 'VSSetup', 'powershell-yaml', 'ps1xmlgen')
+    $PsModules = @('psake', 'PsScriptAnalyzer', 'VSSetup', 'powershell-yaml', 'ps1xmlgen', 'PlatyPS')
 
     $script:PackagesDir = Join-Path $SolutionDir 'packages'
 
