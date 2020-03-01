1eaeccd318c5e3b2e28621e55c38179dd4c75d87
Igor Abade <igor.abade@cloudmotion.com.br>
2020-03-01 04:26:42 -0300

    Fix shell prompt

--- Module/_Private/TfsCmdletsPrompt.ps1
+++ Module/_Private/TfsCmdletsPrompt.ps1
@@ -1,58 +1,60 @@
 Function _TfsCmdletsPrompt
 {
     $promptPrefix = '[Not connected]'
-    $defaultPsPrompt = " $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
+    $defaultPsPrompt = " $($ExecutionContext.SessionState.Path.CurrentLocation)$('>' * ($NestedPromptLevel + 1)) "
+    $backColor = 'DarkGray'
+    $foreColor = 'White'
 
     $server = (Get-TfsConfigurationServer -Current)
 
-    if(-not $server)
+    if($server)
     {
-        return "$promptPrefix $defaultPsPrompt"
-    }
+        $tpc = (Get-TfsTeamProjectCollection -Current)
+        $tp = (Get-TfsTeamProject -Current)
+        $serverName = $server.Name
+        $userName = $server.AuthorizedIdentity.UniqueName
 
-    $tpc = (Get-TfsTeamProjectCollection -Current)
-    $tp = (Get-TfsTeamProject -Current)
-    $serverName = $server.Name
-    $userName = $server.AuthorizedIdentity.UniqueName
+        if ($serverName -like '*.visualstudio.com')
+        {
+            $tpcName = $serverName.SubString(0, $serverName.IndexOf('.'))
+            $promptPrefix = "[AzDev:/$tpcName"
+            $backColor = 'DarkBlue'
+            $foreColor = 'White'
+        }
+        elseif ($serverName -eq 'dev.azure.com')
+        {
+            $tpcName = $server.Uri.Segments[1]
+            $promptPrefix = "[AzDev:/$tpcName"
+            $backColor = 'DarkBlue'
+            $foreColor = 'White'
+        }
+        else
+        {
+            $promptPrefix = "[TFS:/$($server.Uri.Host)/"
+            $backColor = 'DarkMagenta'
+            $foreColor = 'White'
 
-    if ($serverName -like '*.visualstudio.com')
-    {
-        $tpcName = $serverName.SubString(0, $serverName.IndexOf('.'))
-        $promptPrefix = "[AzDev:/"
-        $backColor = 'DarkBlue'
-        $foreColor = 'White'
-    }
-    elseif ($serverName -eq 'dev.azure.com')
-    {
-        $promptPrefix = "[AzDev:/"
-        $backColor = 'DarkBlue'
-        $foreColor = 'White'
-    }
-    else
-    {
-        $promptPrefix = "[TFS:/$($server.Uri.Host)/"
-        $backColor = 'DarkMagenta'
-        $foreColor = 'White'
-    }
+            if ($tpc)
+            {
+                $promptPrefix += "$($tpc.Name)"
+            }
+        }
 
-    if ($tpc)
-    {
-        $promptPrefix += "$($tpc.Name)"
+        if ($tp)
+        {
+            $promptPrefix += "/$($tp.Name)"
+        }
+
+        if($userName)
+        {
+            $promptPrefix += " ($userName)"
+        }
+
+        $promptPrefix += ']'
+
     }
-
-    if ($tp)
-    {
-        $promptPrefix += "/$($tp.Name)"
-    }
-
-    if($userName)
-    {
-        $promptPrefix += " ($userName)"
-    }
-
-    $promptPrefix += ']'
-
-    Write-Host $promptPrefix -NoNewline -ForegroundColor $foreColor -BackgroundColor $backColor
+ 
+    Write-Host -Object $promptPrefix -NoNewline -ForegroundColor $foreColor -BackgroundColor $backColor
 
     return $defaultPsPrompt
 }
