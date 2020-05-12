3ca6e4c5c79c47272606f31c104c08cc050082fb
Igor Abade V. Leite <igor.abade@cloudmotion.com.br>
2020-05-12 14:07:20 -0300

    Add parameter logging support

--- Module/_Private/Log.ps1
+++ Module/_Private/Log.ps1
@@ -29,13 +29,49 @@ Function _Log
     Write-Verbose "[$([DateTime]::Now.ToString('HH:mm:ss.ffff'))] [$caller] $Message"
 }
 
+Function _LogParams
+{
+    Param
+    (
+        [Parameter()]
+        [Hashtable]
+        $Parameters,
+
+        [Parameter()]
+        [string]
+        $Caller,
+
+        [Parameter()]
+        [switch]
+        $Force
+    )
+
+    if(($VerbosePreference -ne 'Continue') -and (-not $Force.IsPresent))
+    {
+        # No verbose set. Exit now to avoid expensive/unnecessary calls to Get-PSCallStack and Write-Verbose
+        return
+    }
+
+    if(-not $Parameters)
+    {
+        $Parameters = Get-Variable -Name 'PSBoundParameters' -Scope 1 -ValueOnly
+    }
+
+    if(-not $Caller)
+    {
+        $Caller = _GetLogCallStack
+    }
+
+    _Log "ARGS: $(_DumpObj $Parameters -Depth 1)" -Caller $Caller -Force:$Force.IsPresent
+}
+
 Function _GetLogCallStack
 {
     $cs = [System.Collections.Stack]::new()
 
     foreach($frame in Get-PSCallStack)
     {
-        if ($frame.Command -in @('_Log', '_GetLogCallStack', '', $null))
+        if ($frame.Command -in @('_Log', '_LogParams', '_GetLogCallStack', '', $null))
         {
             continue
         }
