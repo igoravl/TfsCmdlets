afcf9fff08ed0b1a2f29926c352e0623f8e54b58
Igor Abade <igoravl@gmail.com>
2020-06-01 17:49:33 -0300

    Add lazy property support

---
+++ CSharp/TfsCmdlets.Common/Util/LazyProperty.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using TfsCmdlets.Extensions;
+
+namespace TfsCmdlets.Util
+{
+    /// <summary>
+    /// Supports the creation of lazy-loaded ScriptProperty properties
+    /// </summary>
+    public static class LazyProperty
+    {
+        /// <summary>
+        /// Gets the value of the specified lazy-loaded property
+        /// </summary>
+        /// <param name="obj">The source PSObject that owns the property</param>
+        /// <param name="property">The property name</param>
+        /// <param name="sb">The scriptblock that is evaluated when the property 
+        ///     needs to be loaded. It must return the value to be assigned to the property.</param>
+        /// <returns>The value of the property</returns>
+        public static object Get(PSObject obj, string property, ScriptBlock sb)
+        {
+            var propertyBag = obj.Properties.FirstOrDefault(
+                p => p.Name.Equals("__PropertyBag"))?.Value as Dictionary<string,object>;
+
+            if(propertyBag == null)
+            {
+                propertyBag = new Dictionary<string,object>();
+                obj.AddNoteProperty("__PropertyBag", propertyBag);
+            }
+
+            if(propertyBag.ContainsKey(property))
+            {
+                return propertyBag[property];
+            }
+
+            var value = propertyBag[property] = sb.InvokeWithContext(null, new List<PSVariable>(){
+                new PSVariable("_", obj)
+            });
+
+            return value;
+        }
+    }
+}
