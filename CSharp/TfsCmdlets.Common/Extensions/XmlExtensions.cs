1cfb9092e91580b78dd186976da75e21cce93145
Igor Abade <igoravl@gmail.com>
2020-06-09 13:02:00 -0300

    Add XML extensions

---
+++ CSharp/TfsCmdlets.Common/Extensions/XmlExtensions.cs
@@ -0,0 +1,27 @@
+using System.Xml;
+using System.Xml.Linq;
+
+namespace TfsCmdlets.Extensions
+{
+    public static class DocumentExtensions
+    {
+        public static XmlDocument ToXmlDocument(this XDocument xDocument)
+        {
+            var xmlDocument = new XmlDocument();
+            using (var xmlReader = xDocument.CreateReader())
+            {
+                xmlDocument.Load(xmlReader);
+            }
+            return xmlDocument;
+        }
+
+        public static XDocument ToXDocument(this XmlDocument xmlDocument)
+        {
+            using (var nodeReader = new XmlNodeReader(xmlDocument))
+            {
+                nodeReader.MoveToContent();
+                return XDocument.Load(nodeReader);
+            }
+        }
+    }
+}
