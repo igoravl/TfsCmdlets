68f18caeef28875bce560eb53bfd5ba6c6745ff1
Igor Abade <igoravl@gmail.com>
2020-06-10 10:03:10 -0300

    Document Get-RestClient

--- CSharp/TfsCmdlets.Common/Cmdlets/RestApi/GetRestClient.cs
+++ CSharp/TfsCmdlets.Common/Cmdlets/RestApi/GetRestClient.cs
@@ -8,17 +8,38 @@ using TfsCmdlets.Util;
 
 namespace TfsCmdlets.Cmdlets.RestApi
 {
+    /// <summary>
+    /// Gets an Azure DevOps HTTP Client object instance.
+    /// </summary>
+    /// <remarks>
+    /// Connection objects (Microsoft.VisualStudio.Services.Client.VssConnection in PowerShell Core, 
+    /// Microsoft.TeamFoundation.Client.TfsTeamProjectCollection in Windows PowerShell) provide access to 
+    /// many HTTP client objects such as Microsoft.TeamFoundation.WorkItemTracking.WebApi.WorkItemTrackingHttpClient 
+    /// that wrap many of the REST APIs exposed by Azure DevOps. Those clients inherit the authentication 
+    /// information supplied by their parent connection object and can be used as a more convenient mechanism 
+    /// to issue API calls.
+    /// </remarks>
     [Cmdlet(VerbsCommon.Get, "TfsRestClient", DefaultParameterSetName = "Get by collection")]
     [OutputType(typeof(VssHttpClientBase))]
     public class GetRestClient : BaseCmdlet
     {
+        /// <summary>
+        /// Specifies the full type name (optionally including its assembly name) of the HTTP Client 
+        /// class to return.
+        /// </summary>
         [Parameter(Mandatory = true, Position = 0)]
         [Alias("Type")]
         public string TypeName { get; set; }
 
+        /// <summary>
+        /// HELP_PARAM_COLLECTION
+        /// </summary>
         [Parameter(ParameterSetName = "Get by collection")]
         public object Collection { get; set; }
 
+        /// <summary>
+        /// HELP_PARAM_SERVER
+        /// </summary>
         [Parameter(ParameterSetName = "Get by server", Mandatory = true)]
         public object Server { get; set; }
 
