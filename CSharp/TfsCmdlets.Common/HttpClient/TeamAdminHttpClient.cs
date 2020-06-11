662768f4bb0050f98047b2292b9484ee9eabdbe5
Igor Abade <igoravl@gmail.com>
2020-06-11 19:09:21 -0300

    Add class documentation

--- CSharp/TfsCmdlets.Common/HttpClient/TeamAdminHttpClient.cs
+++ CSharp/TfsCmdlets.Common/HttpClient/TeamAdminHttpClient.cs
@@ -6,13 +6,22 @@ using Microsoft.VisualStudio.Services.Common;
 
 namespace TfsCmdlets.HttpClient
 {
+    /// <summary>
+    /// Custom HTTP Client to handle team administrator management
+    /// </summary>
     public class TeamAdminHttpClient : GenericHttpClient
     {
+        /// <summary>
+        /// Adds an administrator to a team
+        /// </summary>
         public IEnumerable<TeamAdmin> AddTeamAdmin(Guid projectId, Guid teamId, Guid userId)
         {
             return AddTeamAdmin(projectId.ToString(), teamId, userId);
         }
 
+        /// <summary>
+        /// Adds an administrator to a team
+        /// </summary>
         public IEnumerable<TeamAdmin> AddTeamAdmin(string project, Guid teamId, Guid userId)
         {
             var result = Post<AddTeamAdminRequestData, TeamAdmins>(
@@ -27,11 +36,17 @@ namespace TfsCmdlets.HttpClient
             return result.Admins;
         }
 
+        /// <summary>
+        /// Removes an administrator from a team
+        /// </summary>
         public bool RemoveTeamAdmin(Guid project, Guid teamId, Guid userId)
         {
             return RemoveTeamAdmin(project.ToString(), teamId, userId);
         }
 
+        /// <summary>
+        /// Removes an administrator from a team
+        /// </summary>
         public bool RemoveTeamAdmin(string project, Guid teamId, Guid userId)
         {
             var result = PostForm<RemoveTeamAdminResult>(
@@ -53,25 +68,40 @@ namespace TfsCmdlets.HttpClient
 
         #region Constructors and fields
 
+        /// <summary>
+        /// Creates a new instance of the TeamAdminHttpClient class
+        /// </summary>
         public TeamAdminHttpClient(Uri baseUrl, VssCredentials credentials) : base(baseUrl, credentials)
         {
         }
 
+        /// <summary>
+        /// Creates a new instance of the TeamAdminHttpClient class
+        /// </summary>
         public TeamAdminHttpClient(Uri baseUrl, VssCredentials credentials, VssHttpRequestSettings settings) : base(
             baseUrl, credentials, settings)
         {
         }
 
+        /// <summary>
+        /// Creates a new instance of the TeamAdminHttpClient class
+        /// </summary>
         public TeamAdminHttpClient(Uri baseUrl, VssCredentials credentials, params DelegatingHandler[] handlers) : base(
             baseUrl, credentials, handlers)
         {
         }
 
+        /// <summary>
+        /// Creates a new instance of the TeamAdminHttpClient class
+        /// </summary>
         public TeamAdminHttpClient(Uri baseUrl, HttpMessageHandler pipeline, bool disposeHandler) : base(baseUrl,
             pipeline, disposeHandler)
         {
         }
 
+        /// <summary>
+        /// Creates a new instance of the TeamAdminHttpClient class
+        /// </summary>
         public TeamAdminHttpClient(Uri baseUrl, VssCredentials credentials, VssHttpRequestSettings settings,
             params DelegatingHandler[] handlers) : base(baseUrl, credentials, settings, handlers)
         {
@@ -82,48 +112,123 @@ namespace TfsCmdlets.HttpClient
 
     #region Data Classes
 
+    /// <summary>
+    /// Represents a collection of team administrators
+    /// </summary>
     [DataContract]
     public class TeamAdmins
     {
-        [DataMember(Name = "admins")] public TeamAdmin[] Admins { get; set; }
+        /// <summary>
+        /// Collection of team administrators
+        /// </summary>
+        [DataMember(Name = "admins")] 
+        public TeamAdmin[] Admins { get; set; }
     }
 
+    /// <summary>
+    /// Represents a team administrator
+    /// </summary>
     public class TeamAdmin
     {
+        /// <summary>
+        /// Identity Type
+        /// </summary>
         public string IdentityType { get; set; }
+
+        /// <summary>
+        /// Friendly Display Name
+        /// </summary>
         public string FriendlyDisplayName { get; set; }
+
+        /// <summary>
+        /// Display Name
+        /// </summary>
         public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Sub-header
+        /// </summary>
         public string SubHeader { get; set; }
+
+        /// <summary>
+        /// Team Foundation Id
+        /// </summary>
         public string TeamFoundationId { get; set; }
         public string EntityId { get; set; }
+
+        /// <summary>
+        /// List of Errors
+        /// </summary>
         public object[] Errors { get; set; }
+
+        /// <summary>
+        /// List of Warnings
+        /// </summary>
         public object[] Warnings { get; set; }
+
+        /// <summary>
+        /// User Domain
+        /// </summary>
         public string Domain { get; set; }
+
+        /// <summary>
+        /// User Account Name
+        /// </summary>
         public string AccountName { get; set; }
+
+        /// <summary>
+        /// Is Windows User
+        /// </summary>
         public bool IsWindowsUser { get; set; }
+
+        /// <summary>
+        /// Email Address
+        /// </summary>
         public string MailAddress { get; set; }
 
+        /// <inheritdoc/>
         public override string ToString()
         {
             return $"{FriendlyDisplayName} ({AccountName})";
         }
     }
 
+    /// <summary>
+    /// The request body to submit to the "Add Admin" service
+    /// </summary>
     [DataContract]
     public class AddTeamAdminRequestData
     {
-        [DataMember(Name = "teamId")] public Guid Team { get; set; }
+        /// <summary>
+        /// TeamId
+        /// </summary>
+        [DataMember(Name = "teamId")] 
+        public Guid Team { get; set; }
 
-        [DataMember(Name = "newUsersJson")] public string NewUsers { get; set; }
+        /// <summary>
+        /// List of New Users
+        /// </summary>
+        [DataMember(Name = "newUsersJson")]
+        public string NewUsers { get; set; }
 
+        /// <summary>
+        /// List of Existing Users
+        /// </summary>
         [DataMember(Name = "existingUsersJson")]
         public string ExistingUsers { get; set; }
     }
 
+    /// <summary>
+    /// The request body to submit to the "Remove Admin" service
+    /// </summary>
     [DataContract]
     public class RemoveTeamAdminResult
     {
-        [DataMember(Name = "success")] public bool Success { get; set; }
+        /// <summary>
+        /// Indicates the success of the operation
+        /// </summary>
+        [DataMember(Name = "success")] 
+        public bool Success { get; set; }
     }
 
     #endregion
--- CSharp/TfsCmdlets.Common/Models/GlobalList.cs
+++ CSharp/TfsCmdlets.Common/Models/GlobalList.cs
@@ -20,7 +20,21 @@ namespace TfsCmdlets.Models
         /// </summary>
         public static implicit operator GlobalList(XElement el) => new GlobalList(el);
 
+        /// <summary>
+        /// Creates an empty instance of GlobalList
+        /// </summary>
         public GlobalList() {}
+
+        /// <summary>
+        /// Creates a new instance from the given name and items
+        /// </summary>
+        /// <param name="name">The name of the global list</param>
+        /// <param name="items">The items (content) of the global list</param>
+        internal GlobalList(string name, IEnumerable<string> items)
+        {
+            Name = name;
+            Items = new List<string>(items);
+        }
 
         /// <summary>
         /// Creates an instance of GlobalList from an XElement containing a &lt;GLOBALLIST&gt; element
@@ -43,12 +57,6 @@ namespace TfsCmdlets.Models
         /// </summary>
         public IList<string> Items { get; internal set;} = new List<string>();
         
-        internal GlobalList(string name, IEnumerable<string> items)
-        {
-            Name = name;
-            Items = new List<string>(items);
-        }
-
         /// <inheritdoc/>
         public override string ToString()
         {
@@ -65,32 +73,68 @@ namespace TfsCmdlets.Models
         }
     }
 
+    /// <summary>
+    /// Represents a collection of global lists.
+    /// </summary>
     public class GlobalListCollection: List<GlobalList>
     {
         private static readonly XNamespace _glNs = "http://schemas.microsoft.com/VisualStudio/2005/workitemtracking/globallists";
 
+        /// <summary>
+        /// Converts a collection of global lists to a well-formed &lt;gl:GLOBALLISTS&gt; document
+        /// </summary>
         public static implicit operator XDocument(GlobalListCollection list) => list.ToXml();
 
+        /// <summary>
+        /// Converts a well-formed &lt;gl:GLOBALLISTS&gt; document to a collection of global lists
+        /// </summary>
         public static implicit operator GlobalListCollection(XDocument doc) => new GlobalListCollection(doc);
 
+        /// <summary>
+        /// Creates an empty collection
+        /// </summary>
         public GlobalListCollection(): base() {}
 
+        /// <summary>
+        /// Creates a collection and adds the given global lists to it
+        /// </summary>
+        /// <param name="items">Collection of global lists to add to this instance</param>
         public GlobalListCollection(IEnumerable<GlobalList> items): base(items) {}
 
+        /// <summary>
+        /// Creates a collection and adds the given global list to it
+        /// </summary>
+        /// <param name="item">A single global list to add to this instance</param>
         public GlobalListCollection(GlobalList item): base() => Add(item);
 
+        /// <summary>
+        /// Creates a collection from the given &lt;gl:GLOBALLISTS&gt; document
+        /// </summary>
+        /// <param name="xml">A well-formed &lt;gl:GLOBALLISTS&gt; document</param>
         public GlobalListCollection(string xml): this(XDocument.Parse(xml)) {}
 
+        /// <summary>
+        /// Creates a collection from the given &lt;gl:GLOBALLISTS&gt; document
+        /// </summary>
+        /// <param name="doc">A well-formed &lt;gl:GLOBALLISTS&gt; document</param>
         public GlobalListCollection(XDocument doc)
         {
             AddRange(doc.Descendants("GLOBALLIST").Select(el => new GlobalList(el)));
         }
 
+        /// <summary>
+        /// Returns the XML representation of the contents of this collection, in the form of a 
+        /// &lt;gl:GLOBALLISTS&gt; document
+        /// </summary>
         public override string ToString()
         {
             return ToXml().ToString();
         }
 
+        /// <summary>
+        /// Returns the XML representation of the contents of this collection, in the form of a 
+        /// &lt;gl:GLOBALLISTS&gt; document, as a XDocument object
+        /// </summary>
         public XDocument ToXml()
         {
             return CreateDocument(this.Select(gl => gl.ToXml()));
