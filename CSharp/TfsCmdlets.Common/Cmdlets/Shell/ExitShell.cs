209ad23760c02dbb072b900c2ba490accdd07571
Igor Abade <igoravl@gmail.com>
2020-06-11 00:38:18 -0300

    Document Exit-Shell

--- CSharp/TfsCmdlets.Common/Cmdlets/Shell/ExitShell.cs
+++ CSharp/TfsCmdlets.Common/Cmdlets/Shell/ExitShell.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
 using System.Management.Automation;
-using TfsCmdlets.Extensions;
 
 namespace TfsCmdlets.Cmdlets.Shell
 {
+    /// <summary>
+    /// Deactivates the Azure DevOps Shell
+    /// </summary>
     [Cmdlet(VerbsCommon.Exit, "TfsShell")]
     public class ExitShell : BaseCmdlet
     {
