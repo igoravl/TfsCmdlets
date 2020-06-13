e127a03a5f9d555a1899c46ede8a9b625467f3f2
Igor Abade <igoravl@gmail.com>
2020-06-12 21:56:42 -0300

    Add first argument completers (Project, Team)

---
+++ PS/Private/ArgumentCompleters.psm1
@@ -0,0 +1,76 @@
+# Project
+
+Register-ArgumentCompleter -ParameterName Project -Verbose -ScriptBlock {
+    
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    if ($commandName -notlike '*-Tfs*') {
+        return
+    }
+
+    if ($fakeBoundParameter['Collection']) {
+        $tpc = Get-TfsTeamProjectCollection -Collection $fakeBoundParameter['Collection'] -Server $fakeBoundParameter['Server']
+    }
+    elseif ((Get-TfsTeamProjectCollection -Current)) {
+        $tpc = (Get-TfsTeamProjectCollection -Current)
+    }
+    else {
+        return
+    }
+ 
+    if ($tpc) {
+        return Get-TfsTeamProject -Project "$wordToComplete*" -Collection $tpc | Select-Object -ExpandProperty Name | Sort-Object | _EscapeArgumentValue
+    }
+}
+
+# Team
+
+Register-ArgumentCompleter -ParameterName Team -Verbose -ScriptBlock {
+    
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    if ($commandName -notlike '*-Tfs*') {
+        return
+    }
+
+    if ($fakeBoundParameter['Collection']) {
+        $tpc = Get-TfsTeamProjectCollection -Collection $fakeBoundParameter['Collection'] -Server $fakeBoundParameter['Server']
+    }
+    elseif ((Get-TfsTeamProjectCollection -Current)) {
+        $tpc = (Get-TfsTeamProjectCollection -Current)
+    }
+    else {
+        return
+    }
+ 
+    if ($fakeBoundParameter['Project']) {
+        $tp = Get-TfsTeamProject -Collection $fakeBoundParameter['Project'] -Collection $tpc
+    }
+    elseif ((Get-TfsTeamProject -Current)) {
+        $tp = (Get-TfsTeamProject -Current)
+    }
+    else {
+        return
+    }
+
+    return Get-TfsTeam -Team "$wordToComplete*" -Project $tp -Collection $tpc | Select-Object -ExpandProperty Name | Sort-Object | _EscapeArgumentValue
+}
+
+Function _EscapeArgumentValue {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(ValueFromPipeline = $true)]
+        [string]
+        $InputObject    
+    )
+
+    Process {
+        if ($InputObject.Contains(' ') -or $InputObject.Contains("'") -or $InputObject.Contains('"')) {
+            $InputObject = "'" + $InputObject.Replace("'", "''") + "'"
+        }
+        
+        return $InputObject
+    }
+}
+
