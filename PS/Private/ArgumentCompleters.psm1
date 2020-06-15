5226c2f12fa398183c7b385e4c2082f6f0e54872
Igor Abade <igoravl@gmail.com>
2020-06-15 19:40:22 -0300

    Add area/iteration argument completer

--- PS/Private/ArgumentCompleters.psm1
+++ PS/Private/ArgumentCompleters.psm1
@@ -56,6 +56,39 @@ Register-ArgumentCompleter -ParameterName Team -Verbose -ScriptBlock {
     return Get-TfsTeam -Team "$wordToComplete*" -Project $tp -Collection $tpc | Select-Object -ExpandProperty Name | Sort-Object | _EscapeArgumentValue
 }
 
+# Area / Iteration
+
+Register-ArgumentCompleter -ParameterName Node -Verbose -ScriptBlock {
+    
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    if ($commandName -notlike '*-Tfs*') {
+        return
+    }
+
+    if ($fakeBoundParameter['Collection']) {
+        $tpc = Get-TfsTeamProjectCollection -Collection $fakeBoundParameter['Collection'] -Server $fakeBoundParameter['Server']
+    }
+    elseif ((Get-TfsTeamProjectCollection -Current)) {
+        $tpc = (Get-TfsTeamProjectCollection -Current)
+    }
+    else {
+        return
+    }
+ 
+    if ($fakeBoundParameter['Project']) {
+        $tp = Get-TfsTeamProject -Collection $fakeBoundParameter['Project'] -Collection $tpc
+    }
+    elseif ((Get-TfsTeamProject -Current)) {
+        $tp = (Get-TfsTeamProject -Current)
+    }
+    else {
+        return
+    }
+
+    return Get-TfsArea -Node "\$wordToComplete*" -Project $tp -Collection $tpc | Select-Object -ExpandProperty RelativePath | Sort-Object | _EscapeArgumentValue
+}
+
 Function _EscapeArgumentValue {
     [CmdletBinding()]
     param
