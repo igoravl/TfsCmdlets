e3e47e8984a1e9d073eefe66d341a016483c2c9a
Igor Abade <igoravl@gmail.com>
2020-06-16 14:35:54 -0300

    Migrate Remove-Team

--- CSharp/TfsCmdlets.Common/Cmdlets/Team/RemoveTeam.cs
+++ CSharp/TfsCmdlets.Common/Cmdlets/Team/RemoveTeam.cs
@@ -1,71 +1,45 @@
-/*
-.SYNOPSIS
-    Deletes a team.
-
-.PARAMETER Project
-    Specifies either the name of the Team Project or a previously initialized Microsoft.TeamFoundation.WorkItemTracking.Client.Project object to connect to. If omitted, it defaults to the connection opened by Connect-TfsTeamProject (if any). 
-
-For more details, see the Get-TfsTeamProject cmdlet.
-
-.PARAMETER Collection
-    Specifies either a URL/name of the Team Project Collection to connect to, or a previously initialized TfsTeamProjectCollection object. 
-
-When using a URL, it must be fully qualified. The format of this string is as follows: 
-
-http[s]://<ComputerName>:<Port>/[<TFS-vDir>/]<CollectionName> 
-
-Valid values for the Transport segment of the URI are HTTP and HTTPS. If you specify a connection URI with a Transport segment, but do not specify a port, the session is created with standards ports: 80 for HTTP and 443 for HTTPS. 
-
-To connect to a Team Project Collection by using its name, a TfsConfigurationServer object must be supplied either via -Server argument or via a previous call to the Connect-TfsConfigurationServer cmdlet. 
-
-For more details, see the Get-TfsTeamProjectCollection cmdlet.
-
-.INPUTS
-    Microsoft.TeamFoundation.Client.TeamFoundationTeam
-    System.String
-*/
-
 using System.Management.Automation;
 using Microsoft.TeamFoundation.Core.WebApi;
+using TfsCmdlets.Extensions;
 
 namespace TfsCmdlets.Cmdlets.Team
 {
+    /// <summary>
+    /// Deletes a team.
+    /// </summary>
     [Cmdlet(VerbsCommon.Remove, "TfsTeam", SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.High)]
     [OutputType(typeof(WebApiTeam))]
-    public class RemoveTeam : BaseCmdlet
+    public class RemoveTeam : RemoveCmdletBase<Models.Team>
     {
-        /*
-                [Parameter(Position=0, ValueFromPipeline=true)]
-                [Alias("Name")]
-                [ValidateScript({(_ is string]) || (_ is [Microsoft.TeamFoundation.Core.WebApi.WebApiTeam])}) 
-                [SupportsWildcards()]
-                public object Team { get; set; } = "*";
+        /// <summary>
+        /// HELP_PARAM_TEAM
+        /// </summary>
+        [Parameter(Position = 0, ValueFromPipeline = true)]
+        [Alias("Name")]
+        [SupportsWildcards()]
+        public object Team { get; set; }
+    }
 
-                [Parameter()]
-                public object Project { get; set; }
-
-                [Parameter()]
-                public object Collection { get; set; }
-
+    partial class TeamDataService
+    {
         /// <summary>
         /// Performs execution of the command
         /// </summary>
-        protected override void ProcessRecord()
-            {
-                if(Team.ProjectName) {Project = Team.ProjectName}; tpc = this.GetProject();; if (! tpc || (tpc.Count != 1)) {throw new Exception($"Invalid or non-existent team project {Project}."}; tp = tpc.Store.TeamProjectCollection)
-                t = Get-TfsTeam -Team Team -Project Project -Collection Collection
+        protected override void DoRemoveItem()
+        {
+            var (_, tp) = GetCollectionAndProject();
+            var teams = GetItems<Models.Team>();
 
-                if (! ShouldProcess(t.Name, "Delete team"))
+            foreach (var t in teams)
+            {
+                if (!ShouldProcess(tp, $"Delete team '{t.Name}'"))
                 {
-                    return
+                    continue;
                 }
 
-                var client = GetClient<Microsoft.TeamFoundation.Core.WebApi.TeamHttpClient>();
-                task = client.DeleteTeamAsync(tp.Name, t.Name)
-
-                result = task.Result; if(task.IsFaulted) { _throw new Exception("Error deleting team" task.Exception.InnerExceptions })
+                GetClient<TeamHttpClient>().DeleteTeamAsync(tp.Name, t.Name)
+                    .Wait($"Error deleting team {t.Name}");
             }
         }
-        */
     }
 }
