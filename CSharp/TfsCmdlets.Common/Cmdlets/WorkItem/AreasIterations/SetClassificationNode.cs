444efbba47e5b89d81998656359f149d24d1238e
Igor Abade <igoravl@gmail.com>
2020-06-23 08:16:08 -0300

    Migrate Set-Iteration

--- CSharp/TfsCmdlets.Common/Cmdlets/WorkItem/AreasIterations/SetClassificationNode.cs
+++ CSharp/TfsCmdlets.Common/Cmdlets/WorkItem/AreasIterations/SetClassificationNode.cs
@@ -1,99 +1,95 @@
+using System;
+using System.Collections.Generic;
 using System.Management.Automation;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using TfsCmdlets.Extensions;
+using TfsCmdlets.Models;
+using TfsCmdlets.Util;
 
 namespace TfsCmdlets.Cmdlets.WorkItem.AreasIterations
 {
-    [Cmdlet(VerbsCommon.Set, "TfsClassificationNode", ConfirmImpact = ConfirmImpact.Medium, SupportsShouldProcess = true)]
+    /// <summary>
+    /// Modifies the dates of an iteration.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Set, "TfsIteration")]
     [OutputType(typeof(WorkItemClassificationNode))]
-    public class SetClassificationNode : CmdletBase
+    public class SetIteration : SetClassificationNode
     {
         /// <summary>
-        /// Performs execution of the command
+        /// HELP_PARAM_ITERATION
         /// </summary>
-        protected override void ProcessRecord() => throw new System.NotImplementedException();
+        [Parameter(Position = 0, ValueFromPipeline = true)]
+        [SupportsWildcards()]
+        [ValidateNotNullOrEmpty]
+        [Alias("Path", "Iteration")]
+        public override object Node { get; set; }
 
-        //         [Parameter(Position=0, Mandatory=true, ValueFromPipeline=true, ValueFromPipelineByPropertyName=true)]
-        //         [Alias("Area")]
-        //         [Alias("Iteration")]
-        //         [Alias("Path")]
-        //         [ValidateScript({(_ is string]) || (_ is [Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.WorkItemClassificationNode])}) 
-        //         [SupportsWildcards()]
-        //         public object Node { get; set; }
+        /// <summary>
+        /// Specifies the start date of the iteration. To clear the start date, set it to $null. Note that when clearing a date, 
+        /// both must be cleared at the same time (i.e. setting both StartDate and FinishDate to $null).
+        /// </summary>
+        [Parameter(Mandatory = true)]
+        public DateTime? StartDate { get; set; }
 
-        //         [Parameter()]
-        //         public string StructureGroup { get; set; }
+        /// <summary>
+        /// Sets the finish date of the iteration. To clear the finish date, set it to $null. Note that when clearing a date, 
+        /// both must be cleared at the same time (i.e. setting both StartDate and FinishDate to $null).
+        /// </summary>
+        [Parameter(Mandatory = true)]
+        public DateTime? FinishDate { get; set; }
 
-        //         [Parameter()]
-        //         public int MoveBy { get; set; }
+        /// <inheritdoc/>
+        protected override TreeStructureGroup StructureGroup => TreeStructureGroup.Iterations;
+    }
 
-        //         [Parameter()]
-        //         [Nullable[DateTime]]
-        //         StartDate,
+    /// <summary>
+    /// Base implementation for Set-Iteration
+    /// </summary>
+    public abstract class SetClassificationNode : SetCmdletBase<ClassificationNode>
+    {
+        /// <summary>
+        /// Specifies the name and/or path of the node (area or iteration)
+        /// </summary>
+        public abstract object Node { get; set; }
 
-        //         [Parameter()]
-        //         [Nullable[DateTime]]
-        //         FinishDate,
+        /// <summary>
+        /// Indicates the type of structure (area or iteration)
+        /// </summary>
+        [Parameter()]
+        protected abstract TreeStructureGroup StructureGroup { get; }
+    }
 
-        //         [Parameter()]
-        //         public object Project { get; set; }
+    partial class ClassificationNodeDataService
+    {
+        protected override ClassificationNode DoSetItem()
+        {
+            var nodeToSet = GetItem<ClassificationNode>();
+            var startDate = GetParameter<DateTime?>(nameof(SetIteration.StartDate));
+            var finishDate = GetParameter<DateTime?>(nameof(SetIteration.FinishDate));
+            var structureGroup = GetParameter<TreeStructureGroup>("StructureGroup");
 
-        //         [Parameter()]
-        //         public object Collection { get; set; }
+            ErrorUtil.ThrowIfNotFound(nodeToSet, nameof(SetIteration.Node), GetParameter<string>(nameof(SetIteration.Node)));
 
-        //         [Parameter()]
-        //         public SwitchParameter Passthru { get; set; }
+            var (_, tp) = GetCollectionAndProject();
 
-        // /// <summary>
-        // /// Performs execution of the command
-        // /// </summary>
-        // protected override void ProcessRecord()
-        //     {
-        //         if(! (PSBoundParameters.ContainsKey("StructureGroup"))){if (MyInvocation.InvocationName -like "*Area"){StructureGroup = Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.TreeStructureGroup.Areas}elseif (MyInvocation.InvocationName -like "*Iteration"){StructureGroup = Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.TreeStructureGroup.Iterations}else{throw new Exception("Invalid or missing StructureGroup argument"}};PSBoundParameters["StructureGroup"] = StructureGroup)
+            var client = GetClient<WorkItemTrackingHttpClient>();
 
-        //         nodeToSet = Get-TfsClassificationNode -Node Node -StructureGroup StructureGroup -Project Project -Collection Collection
+            if (!ShouldProcess(tp, $"Set dates on iteration '{nodeToSet.RelativePath}'")) return null;
 
-        //         if (! nodeToSet)
-        //         {
-        //             throw new Exception($"Invalid or non-existent node {Node}")
-        //         }
+            var patch = new WorkItemClassificationNode()
+            {
+                Attributes = new Dictionary<string, object>()
+                {
+                    ["startDate"] = startDate,
+                    ["finishDate"] = finishDate
+                }
+            };
 
-        //         tp = this.GetProject();; if (! tp || (tp.Count != 1)) {throw new Exception($"Invalid or non-existent team project {Project}."}; tpc = tp.Store.TeamProjectCollection)
+            var result = client.UpdateClassificationNodeAsync(patch, tp.Name, structureGroup, nodeToSet.RelativePath.Substring(1))
+                .GetResult($"Error setting dates on iteration '{nodeToSet.FullPath}'");
 
-        //         var client = GetClient<Microsoft.TeamFoundation.WorkItemTracking.WebApi.WorkItemTrackingHttpClient>();
-
-        //         if (PSBoundParameters.ContainsKey("MoveBy"))
-        //         {
-        //             Write-Warning "Reordering of areas/iterations is deprecated, as Azure DevOps UX keeps areas and iterations properly sorted. MoveBy argument ignored."
-        //         }
-
-        //         if (StructureGroup = = "Iterations" && (PSBoundParameters.ContainsKey($"StartDate") || {PSBoundParameters}.ContainsKey("FinishDate")))
-        //         {
-        //             if (! (PSBoundParameters.ContainsKey($"StartDate") && {PSBoundParameters}.ContainsKey("FinishDate")))
-        //             {
-        //                 throw new Exception("When setting iteration dates, both start and finish dates must be supplied.")
-        //             }
-
-        //             if(ShouldProcess(nodeToSet.RelativePath, $"Set iteration start date to "{StartDate}" and finish date to "FinishDate""))
-        //             {
-        //                 patch = new Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.WorkItemClassificationNode() -Property @{
-        //                     attributes = _NewDictionary @([string], [object]) @{
-        //                         startDate = StartDate
-        //                         finishDate = FinishDate
-        //                     }
-        //                 }
-
-        //                 task = client.UpdateClassificationNodeAsync(patch, tp.Name, structureGroup, nodeToSet.RelativePath.SubString(1)); result = task.Result; if(task.IsFaulted) { _throw new Exception( $"Error setting dates on iteration "{{nodeToSet}.FullPath}"" task.Exception.InnerExceptions })
-        //             }
-        //         }
-
-        //         if(Passthru)
-        //         {
-        //             WriteObject(Get-TfsClassificationNode -Node Node -StructureGroup StructureGroup -Project Project -Collection Collection); return;
-        //         }
-        //     }
-        // }
-
-        // Set-Alias -Name Set-TfsArea -Value Set-TfsClassificationNode
-        // Set-Alias -Name Set-TfsIteration -Value Set-TfsClassificationNode
+            return new ClassificationNode(result, tp.Name, client);
+        }
     }
 }
