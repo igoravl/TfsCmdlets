a44fc208379af358c07f872528fab56532012e3c
Igor Abade <igoravl@gmail.com>
2020-07-18 13:59:10 -0300

    Migrate Get-ServiceHookSubscription

--- CSharp/TfsCmdlets.Common/Cmdlets/ServiceHook/GetServiceHookSubscription.cs
+++ CSharp/TfsCmdlets.Common/Cmdlets/ServiceHook/GetServiceHookSubscription.cs
@@ -1,34 +1,96 @@
+using System.Collections.Generic;
 using System.Management.Automation;
 using Microsoft.VisualStudio.Services.ServiceHooks.WebApi;
+using TfsCmdlets.Services;
+using WebApiConsumer = Microsoft.VisualStudio.Services.ServiceHooks.WebApi.Consumer;
+using WebApiPublisher = Microsoft.VisualStudio.Services.ServiceHooks.WebApi.Publisher;
+using WebApiSubscription = Microsoft.VisualStudio.Services.ServiceHooks.WebApi.Subscription;
+using TfsCmdlets.Extensions;
+using System.Linq;
+using System;
 
 namespace TfsCmdlets.Cmdlets.ServiceHook
 {
+    /// <summary>
+    /// Gets one or more service hook subscriptions
+    /// </summary>
     [Cmdlet(VerbsCommon.Get, "TfsServiceHookSubscription")]
-    [OutputType(typeof(Subscription))]
-    public class GetServiceHookSubscription : CmdletBase
+    [OutputType(typeof(WebApiSubscription))]
+    public class GetServiceHookSubscription : GetCmdletBase<WebApiSubscription>
     {
         /// <summary>
-        /// Performs execution of the command
+        /// Specifies the name ("action description") of the subscription. Wildcards are supported. 
+        /// When omitted, returns all service hook subscriptions in the given 
+        /// team project collection.
         /// </summary>
-        protected override void DoProcessRecord() => throw new System.NotImplementedException();
+        [Parameter(Position = 0)]
+        [Alias("Name")]
+        public object Subscription { get; set; } = "*";
 
-        //         [Parameter(Position=0)]
-        //         [Alias("Name")]
-        //         public string Subscription { get; set; } = "*";
+        /// <summary>
+        /// Specifies the name or ID of the service hook publisher to filter subscriptions by.
+        /// When omitted, returns all subscriptions regardless of their publishers.
+        /// </summary>
+        [Parameter()]
+        public object Publisher { get; set; }
 
-        //         [Parameter()]
-        //         public object Collection { get; set; }
+        /// <summary>
+        /// Specifies the name or ID of the service hook consumer to filter subscriptions by. 
+        /// When omitted, returns all subscriptions regardless of their consumers.
+        /// </summary>
+        [Parameter()]
+        public object Consumer { get; set; }
 
-        //         /// <summary>
-        //         /// Performs execution of the command
-        //         /// </summary>
-        //         protected override void DoProcessRecord()
-        //     {
-        //         tpc = Get-TfsTeamProjectCollection -Collection Collection; if (! tpc || (tpc.Count != 1)) {throw new Exception($"Invalid or non-existent team project collection {Collection}."})
-        //         var client = GetClient<Microsoft.VisualStudio.Services.ServiceHooks.WebApi.ServiceHooksPublisherHttpClient>();
+        /// <summary>
+        /// Specifies the event type to filter subscriptions by. 
+        /// When omitted, returns all subscriptions regardless of their event types.
+        /// </summary>
+        [Parameter()]
+        public object EventType { get; set; }
+    }
 
-        //         client.QuerySubscriptionsAsync().Result
-        //     }
-        // }
+    [Exports(typeof(WebApiSubscription))]
+    internal partial class ServiceHookSubscriptionDataService : BaseDataService<WebApiSubscription>
+    {
+        protected override IEnumerable<WebApiSubscription> DoGetItems()
+        {
+            var client = GetClient<ServiceHooksPublisherHttpClient>();
+            var subscription = GetParameter<object>(nameof(GetServiceHookSubscription.Subscription));
+            var eventType = GetParameter<string>(nameof(GetServiceHookSubscription.EventType));
+
+            var publisher = HasParameter(nameof(GetServiceHookSubscription.Publisher))?
+                 GetItem<WebApiPublisher>(): null;
+            var consumer = HasParameter(nameof(GetServiceHookSubscription.Consumer))?
+                GetItem<WebApiConsumer>(): null;
+
+            while (true) switch (subscription)
+                {
+                    case WebApiSubscription p:
+                        {
+                            yield return p;
+                            yield break;
+                        }
+                    case string s:
+                        {
+                            var query = new SubscriptionsQuery() {
+                                PublisherId = publisher?.Id,
+                                ConsumerId = consumer?.Id,
+                                EventType = eventType
+                            };
+                            
+                            var result = client.QuerySubscriptionsAsync(query)
+                                .GetResult("Error getting service hook subscriptions")
+                                .Results
+                                .Where(sub => sub.ActionDescription.IsLike(s));
+
+                            foreach (var r in result) yield return r;
+                            yield break;
+                        }
+                    default:
+                        {
+                            throw new ArgumentException($"Invalid or non-existent Subscription '{subscription}'");
+                        }
+                }
+        }
     }
 }
