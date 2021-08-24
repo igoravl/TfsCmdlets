d56d25835cd1638a961279ec7f8d8955a0019d84
Igor Abade <igoravl@gmail.com>
2021-08-23 22:25:05 -0300

    Fix .NET unit test execution logic

--- psake.ps1
+++ psake.ps1
@@ -60,7 +60,7 @@ Task Package -Depends Build, AllTests, RemoveEmptyFolders, PackageNuget, Package
 Task Build -Depends CleanOutputDir, CreateOutputDir, BuildLibrary, GenerateHelp, CopyFiles, GenerateTypesXml, GenerateFormatXml, UpdateModuleManifest, UnitTests, GenerateDocs {
 }
 
-Task Test -Depends Build, UnitTests, AllTests {
+Task Test -Depends UnitTests, AllTests {
 }
 
 Task CleanOutputDir -PreCondition { -not $Incremental } {
@@ -208,7 +208,7 @@ Task UpdateModuleManifest {
 
 Task UnitTests -PreCondition { -not $SkipTests }  {
 
-    #Exec { Invoke-Pester -Path $TestsDir -OutputFile (Join-Path $OutDir TestResults-Unit.xml) -OutputFormat NUnitXml -PesterOption (New-PesterOption -IncludeVSCodeMarker ) -Strict -Show ([Pester.OutputTypes]'Failed,Summary') -EnableExit:$IsCI -ExcludeTag Correctness, Integration, PSCore, PSDesktop }
+    Exec { dotnet test $SolutionDir/TfsCmdlets.Tests.UnitTests/TfsCmdlets.Tests.UnitTests.csproj -f $TargetFrameworks.Core --filter "Platform!=Desktop&Platform!=Core" --logger:"console;verbosity=detailed" --logger "trx;LogFileName=$OutDir/TfsCmdlets.UnitTests.trx" > $OutDir/TfsCmdlets.UnitTests.log }
 }
 
 Task AllTests -PreCondition { -not $SkipTests } {
