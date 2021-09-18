65a767cfdcd5c65e2c0e8a23a6640d3b49083ae0
Igor Abade <igoravl@gmail.com>
2021-09-18 00:39:12 -0300

    Migrate ExitShell

---
+++ CSharp/TfsCmdlets/Commands/Shell/ExitShell.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Composition;
+using System.IO;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using TfsCmdlets.Models;
+using TfsCmdlets.Services;
+
+namespace TfsCmdlets.Commands.Shell
+{
+    [Command]
+    internal class ExitShell : CommandBase
+    {
+        private IPowerShellService PowerShell { get; }
+
+        public override object InvokeCommand(ParameterDictionary parameters)
+        {
+           if(!EnterShell.IsInShell) return null;
+
+           if (!string.IsNullOrEmpty(EnterShell.PrevShellTitle))
+           {
+               PowerShell.WindowTitle = EnterShell.PrevShellTitle;
+           }
+
+           if (EnterShell.PrevPrompt != null)
+           {
+               PowerShell.InvokeScript("Set-Content function:prompt $OldPrompt", new Dictionary<string,object>() {
+                   ["OldPrompt"] = EnterShell.PrevPrompt
+               });
+           }
+
+           EnterShell.IsInShell = false;
+
+           PowerShell.InvokeScript("Clear-Host");
+
+           return null;
+        }
+
+        [ImportingConstructor]
+        public ExitShell(IPowerShellService powerShell, ILogger logger)
+        : base(logger)
+        {
+            PowerShell = powerShell;
+        }
+    }
+}
