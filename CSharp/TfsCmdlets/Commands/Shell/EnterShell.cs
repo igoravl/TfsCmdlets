be4ccab7dfee442b5d8a6e5b26da2ec70bfc0f4b
Igor Abade <igoravl@gmail.com>
2021-09-18 00:33:57 -0300

    Internalize PS prompt code

--- CSharp/TfsCmdlets/Commands/Shell/EnterShell.cs
+++ CSharp/TfsCmdlets/Commands/Shell/EnterShell.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Composition;
 using System.IO;
 using System.Management.Automation;
@@ -15,10 +17,7 @@ namespace TfsCmdlets.Commands.Shell
 
         public override object InvokeCommand(ParameterDictionary parameters)
         {
-            if (IsInShell)
-            {
-                return null;
-            }
+            if (IsInShell) return null;
 
             var doNotClearHost = parameters.Get<bool>("DoNotClearHost");
             var noLogo = parameters.Get<bool>("NoLogo");
@@ -33,10 +32,9 @@ namespace TfsCmdlets.Commands.Shell
 
             PowerShell.WindowTitle = windowTitle;
 
-            var promptCode = File.ReadAllText(Path.Combine(
-                   PowerShell.Module.ModuleBase,
-                   "Prompt.ps1"
-               ));
+            const string promptCode = @"
+Function prompt { return ([TfsCmdlets.ShellHelper]::GetPrompt()) + `
+    ""$($ExecutionContext.SessionState.Path.CurrentLocation)$('>' * ($NestedPromptLevel + 1)) ""}";
 
             PowerShell.InvokeScript(promptCode, false, PipelineResultTypes.None, null);
 
@@ -51,8 +49,8 @@ namespace TfsCmdlets.Commands.Shell
             if (!noLogo)
             {
                 PowerShell.WriteObject($"TfsCmdlets: {manifest.Description}");
-                PowerShell.WriteObject($"Version {privateData["Build"]}");
-                PowerShell.WriteObject($"Azure DevOps Client Library version {privateData["TfsClientVersion"]}");
+                PowerShell.WriteObject($"Version {privateData?["Build"] ?? "N/A"}");
+                PowerShell.WriteObject($"Azure DevOps Client Library version {privateData?["TfsClientVersion"] ?? "N/A"}");
             }
 
             // WriteObject($"Loading TfsCmdlets module took {{global}:TfsCmdletsLoadSw.ElapsedMilliseconds}ms."
@@ -103,6 +101,69 @@ namespace TfsCmdlets.Commands.Shell
         : base(logger)
         {
             PowerShell = powerShell;
+        }
+    }
+}
+
+namespace TfsCmdlets
+{
+    public static class ShellHelper
+    {
+        public static string GetPrompt()
+        {
+            const string ANSI_BG_BLUE = "\x1b[44m";
+            const string ANSI_BG_CYAN = "\x1b[46m";
+            const string ANSI_BG_MAGENTA = "\x1b[45m";
+            const string ANSI_BG_GRAY = "\x1b[40;1m";
+            const string ANSI_FG_GRAY = "\x1b[30;1m";
+            const string ANSI_FG_WHITE = "\x1b[37;1m";
+            const string ANSI_RESET = "\x1b[40m\x1b[0m";
+            var NOT_CONNECTED = $"{ANSI_BG_GRAY}{ANSI_FG_GRAY}Not connected{ANSI_RESET}";
+
+            var connections = ServiceLocator.Instance.GetExport<ICurrentConnections>();
+
+            List<string> segments;
+            string colorScheme;
+
+            if (connections.Collection is { } tpc)
+            {
+                colorScheme = tpc.IsHosted ? $"{ANSI_BG_CYAN}{ANSI_FG_WHITE}" : $"{ANSI_BG_MAGENTA}{ANSI_FG_WHITE}";
+
+                segments = new List<string>();
+
+                switch (tpc.Uri.Host.ToLowerInvariant())
+                {
+                    case "dev.azure.com":
+                    {
+                        segments.Add("dev.azure.com");
+                        segments.Add(tpc.DisplayName);
+                        break;
+                    }
+                    case { } s when s.EndsWith(".visualstudio.com"):
+                    {
+                        segments.Add(s);
+                        break;
+                    }
+                    default:
+                    {
+                        segments.Add(tpc.Uri.Host);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                return NOT_CONNECTED;
+            }
+
+            if (connections.Project is { } tp) segments.Add(tp.Name);
+
+            if (connections.Team is { } t) segments.Add(t.Name);
+            
+            var userName = tpc.AuthorizedIdentity.UniqueName ??
+                tpc.AuthorizedIdentity.Properties["Account"].ToString();
+
+            return $"{Environment.NewLine}{colorScheme}[{string.Join(" > ", segments.ToArray())} {ANSI_BG_BLUE}({userName}){colorScheme}]{ANSI_RESET}{Environment.NewLine}";
         }
     }
 }
