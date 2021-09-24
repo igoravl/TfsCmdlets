f610d2bdb3e06004ac7c650ffc422a411264d8bc
Igor Abade <igoravl@gmail.com>
2021-09-24 14:27:57 -0300

    Add NSubstitute

---
+++ CSharp/TfsCmdlets.Tests.UnitTests/Commands/Admin/GetVersion_Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NSubstitute;
+using TfsCmdlets.Commands.Admin;
+using TfsCmdlets.Models;
+using TfsCmdlets.Services;
+using Xunit;
+
+namespace TfsCmdlets.Tests.UnitTests.Commands.Admin
+{
+    public class GetVersion_Tests
+    {
+        [Fact]
+        public void Can_Get_Hosted_Version()
+        {
+            // Arrange
+
+            var fixture = new CommandsFixture();
+
+            var tfsVersionTable = Substitute.For<ITfsVersionTable>();
+            
+            var restApi = Substitute.For<IRestApiService>();
+
+            var powerShell = Substitute.For<IPowerShellService>();
+
+            var data = Substitute.For<IDataManager>();
+            data.GetCollection(null).ReturnsForAnyArgs(fixture.GetAzdoCollection());
+
+            var logger = Substitute.For<ILogger>();
+
+            var parms = new ParameterDictionary();
+
+            var cmd = new GetVersion(tfsVersionTable, restApi, powerShell, data, logger);
+
+            // Act
+
+            var actual = cmd.Invoke(parms).First();
+
+            // Assert
+
+            Assert.True(actual.IsHosted);;
+        }
+    }
+}
---
+++ CSharp/TfsCmdlets.Tests.UnitTests/Commands/CommandsFixture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using TfsCmdlets.Models;
+
+namespace TfsCmdlets.Tests.UnitTests.Commands
+{
+    internal class CommandsFixture
+    {
+        public Connection GetTfsCollection()
+        {
+            return new Connection(
+                new VssConnection(
+                    new Uri("http://tfs.local:8080/tfs/SampleCollection"),
+                    new VssCredentials()));
+        }
+
+        //public Connection GetTfsServer()
+        //{
+        //    return new Connection(
+        //        new VssConnection(
+        //            new Uri("http://tfs.local:8080/tfs/"),
+        //            new VssCredentials()));
+        //}
+
+        public Connection GetAzdoCollection()
+        {
+            return new Connection(
+                new VssConnection(
+                    new Uri("https://dev.azure.com/tfscmdlets-sample-org"),
+                    new VssCredentials()));
+        }
+
+    }
+}
