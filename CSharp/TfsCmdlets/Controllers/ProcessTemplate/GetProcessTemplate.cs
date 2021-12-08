4cb190ca133628f13553980783106678f16d8503
Igor Abade <igoravl@gmail.com>
2021-12-08 00:17:15 -0300

    Refactor GetProcessTemplate controller

--- CSharp/TfsCmdlets/Controllers/ProcessTemplate/GetProcessTemplate.cs
+++ CSharp/TfsCmdlets/Controllers/ProcessTemplate/GetProcessTemplate.cs
@@ -12,84 +12,62 @@ namespace TfsCmdlets.Cmdlets.ProcessTemplate
     /// <summary>
     /// Gets information from one or more process templates.
     /// </summary>
-    [Cmdlet(VerbsCommon.Get, "TfsProcessTemplate")]
-    [OutputType(typeof(WebApiProcess))]
+    [CmdletController(typeof(WebApiProcess))]
     partial class GetProcessTemplateController
     {
-        /// <summary>
-        /// Specifies the name of the process template(s) to be returned. Wildcards are supported. 
-        /// When omitted, all process templates in the given project collection are returned.
-        /// </summary>
-        [Parameter(Position = 0, ParameterSetName = "Get by name")]
-        [Alias("Name")]
-        [SupportsWildcards()]
-        public object ProcessTemplate { get; set; } = "*";
+       public override IEnumerable<WebApiProcess> Invoke()
+       {
+           var process = Parameters.Get<object>(nameof(GetProcessTemplate.ProcessTemplate));
+           var isDefault = Parameters.Get<bool>(nameof(GetProcessTemplate.Default));
 
-        /// <summary>
-        /// Returns the default process template in the given orgnization / project collection.
-        /// </summary>
-        [Parameter(Mandatory = true, ParameterSetName = "Get default process")]
-        public SwitchParameter Default { get; set; }
+           var client = Data.GetClient<ProcessHttpClient>();
+
+           while (true) switch (process)
+               {
+                   case WebApiProcess p:
+                       {
+                           yield return p;
+                           yield break;
+                       }
+                   case string s when s.IsGuid():
+                       {
+                           process = new Guid(s);
+                           continue;
+                       }
+                   case Guid g:
+                       {
+                           yield return client.GetProcessByIdAsync(g)
+                               .GetResult($"Error getting process template '{process}'");
+
+                           yield break;
+                       }
+                   case object o when isDefault:
+                       {
+                           foreach (var proc in client.GetProcessesAsync()
+                               .GetResult($"Error getting process templates")
+                               .Where(p => p.IsDefault))
+                           {
+                               yield return proc;
+                           }
+
+                           yield break;
+                       }
+                   case string s:
+                       {
+                           foreach (var proc in client.GetProcessesAsync()
+                               .GetResult($"Error getting process template '{process}'")
+                               .Where(p => p.Name.IsLike(s)))
+                           {
+                               yield return proc;
+                           }
+
+                           yield break;
+                       }
+                   default:
+                       {
+                           throw new ArgumentException($"Invalid or non-existent process '{process}'");
+                       }
+               }
+       }
     }
-
-    // TODO
-
-    //[Exports(typeof(WebApiProcess))]
-    //internal partial class ProcessDataService : CollectionLevelController<WebApiProcess>
-    //{
-    //    protected override IEnumerable<WebApiProcess> DoGetItems()
-    //    {
-    //        var process = parameters.Get<object>(nameof(GetProcessTemplate.ProcessTemplate));
-    //        var isDefault = parameters.Get<bool>(nameof(GetProcessTemplate.Default));
-
-    //        var client = Data.GetClient<ProcessHttpClient>(parameters);
-
-    //        while (true) switch (process)
-    //            {
-    //                case WebApiProcess p:
-    //                    {
-    //                        yield return p;
-    //                        yield break;
-    //                    }
-    //                case string s when s.IsGuid():
-    //                    {
-    //                        process = new Guid(s);
-    //                        continue;
-    //                    }
-    //                case Guid g:
-    //                    {
-    //                        yield return client.GetProcessByIdAsync(g)
-    //                            .GetResult($"Error getting process template '{process}'");
-
-    //                        yield break;
-    //                    }
-    //                case object o when isDefault:
-    //                    {
-    //                        foreach (var proc in client.GetProcessesAsync()
-    //                            .GetResult($"Error getting process templates")
-    //                            .Where(p => p.IsDefault))
-    //                        {
-    //                            yield return proc;
-    //                        }
-
-    //                        yield break;
-    //                    }
-    //                case string s:
-    //                    {
-    //                        foreach (var proc in client.GetProcessesAsync()
-    //                            .GetResult($"Error getting process template '{process}'")
-    //                            .Where(p => p.Name.IsLike(s)))
-    //                        {
-    //                            yield return proc;
-    //                        }
-
-    //                        yield break;
-    //                    }
-    //                default:
-    //                    {
-    //                        throw new ArgumentException($"Invalid or non-existent process '{process}'");
-    //                    }
-    //            }
-    //    }
-    //}
 }
