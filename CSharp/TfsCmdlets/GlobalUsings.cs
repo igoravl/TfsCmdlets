4b62454f2e4d9ee8d3b817e50d737167e745e22a
Igor Abade <igoravl@gmail.com>
2021-12-11 18:52:17 -0300

    Add more global usings

--- CSharp/TfsCmdlets/GlobalUsings.cs
+++ CSharp/TfsCmdlets/GlobalUsings.cs
@@ -5,3 +5,6 @@ global using System.Text;
 global using System.IO;
 global using System.Linq;
 global using TfsCmdlets.Extensions;
+
+using WebApiWorkItem = Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.WorkItem;
+using WebApiTeamProject = Microsoft.TeamFoundation.Core.WebApi.TeamProject;
