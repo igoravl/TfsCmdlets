d4b6caa3dbaede264605097d165bb5eac9c259eb
Igor Abade <igoravl@gmail.com>
2021-12-11 18:51:33 -0300

    Add WorkItemHistory model class

---
+++ CSharp/TfsCmdlets/Models/IdentityRefWrapper.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Management.Automation;
+using WebApiIdentity = Microsoft.VisualStudio.Services.Identity.Identity;
+using Microsoft.VisualStudio.Services.Identity;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using WebApiIdentityRef = Microsoft.VisualStudio.Services.WebApi.IdentityRef;
+
+namespace TfsCmdlets.Models
+{
+    public class IdentityRefWrapper
+    {
+        public static explicit operator WebApiIdentityRef(IdentityRefWrapper i) => i._inner;
+        public static explicit operator IdentityRefWrapper(WebApiIdentityRef i) => new(i);
+
+        private readonly WebApiIdentityRef _inner;
+
+        internal IdentityRefWrapper(WebApiIdentityRef inner)
+        {
+            _inner = inner;
+        }
+
+        public XmlSchema GetSchema()
+        {
+            return ((System.Xml.Serialization.IXmlSerializable)_inner).GetSchema();
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            ((System.Xml.Serialization.IXmlSerializable)_inner).ReadXml(reader);
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            ((System.Xml.Serialization.IXmlSerializable)_inner).WriteXml(writer);
+        }
+
+        public string GetToken()
+        {
+            return ((Microsoft.VisualStudio.Services.WebApi.ISecuredObject)_inner).GetToken();
+        }
+
+        public Guid NamespaceId => ((Microsoft.VisualStudio.Services.WebApi.ISecuredObject)_inner).NamespaceId;
+
+        public int RequiredPermissions => ((Microsoft.VisualStudio.Services.WebApi.ISecuredObject)_inner).RequiredPermissions;
+
+        public SubjectDescriptor Descriptor
+        {
+            get => _inner.Descriptor;
+            set => _inner.Descriptor = value;
+        }
+
+        public string DisplayName
+        {
+            get => _inner.DisplayName;
+            set => _inner.DisplayName = value;
+        }
+
+        public string Url
+        {
+            get => _inner.Url;
+            set => _inner.Url = value;
+        }
+
+        public ReferenceLinks Links
+        {
+            get => _inner.Links;
+            set => _inner.Links = value;
+        }
+
+        public string Id
+        {
+            get => _inner.Id;
+            set => _inner.Id = value;
+        }
+
+        public string UniqueName
+        {
+            get => _inner.UniqueName;
+            set => _inner.UniqueName = value;
+        }
+
+        public string DirectoryAlias
+        {
+            get => _inner.DirectoryAlias;
+            set => _inner.DirectoryAlias = value;
+        }
+
+        public string ProfileUrl
+        {
+            get => _inner.ProfileUrl;
+            set => _inner.ProfileUrl = value;
+        }
+
+        public string ImageUrl
+        {
+            get => _inner.ImageUrl;
+            set => _inner.ImageUrl = value;
+        }
+
+        public bool IsContainer
+        {
+            get => _inner.IsContainer;
+            set => _inner.IsContainer = value;
+        }
+
+        public bool IsAadIdentity
+        {
+            get => _inner.IsAadIdentity;
+            set => _inner.IsAadIdentity = value;
+        }
+
+        public bool Inactive
+        {
+            get => _inner.Inactive;
+            set => _inner.Inactive = value;
+        }
+
+        public bool IsDeletedInOrigin
+        {
+            get => _inner.IsDeletedInOrigin;
+            set => _inner.IsDeletedInOrigin = value;
+        }
+
+        public string DisplayNameForXmlSerialization
+        {
+            get => _inner.DisplayNameForXmlSerialization;
+            set => _inner.DisplayNameForXmlSerialization = value;
+        }
+
+        public string UrlForXmlSerialization
+        {
+            get => _inner.UrlForXmlSerialization;
+            set => _inner.UrlForXmlSerialization = value;
+        }
+
+        public override string ToString()
+        {
+            return _inner?.DisplayName ?? string.Empty;
+        }
+    }
+}
---
+++ CSharp/TfsCmdlets/Models/WorkItemHistory.cs
@@ -0,0 +1,31 @@
+using System.Management.Automation;
+using System.Runtime.Serialization;
+using TfsCmdlets.Extensions;
+using WebApiWorkItem = Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.WorkItem;
+
+namespace TfsCmdlets.Models
+{
+    /// <summary>
+    /// Represents the history of a given work item
+    /// </summary>
+    [DataContract]
+    public class WorkItemHistoryEntry
+    {
+        public int Id { get; set; }
+        public int Revision { get; set; }
+        public DateTime ChangedDate { get; set; }
+        public string ChangedBy { get; set; }
+        public IEnumerable<WorkItemHistoryChangedField> Changes { get; set; }
+        public object Comment { get; internal set; }
+    }
+
+    [DataContract]
+    public class WorkItemHistoryChangedField
+    {
+        public int Id { get; set; }
+        public int Revision { get; set; }
+        public string ReferenceName { get; set; }
+        public object OriginalValue { get; set; }
+        public object NewValue { get; set; }
+    }
+}
