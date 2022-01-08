de4a50896bbdf2885612a08d6e6a224c2356580d
Igor Abade <igoravl@gmail.com>
2022-01-08 00:06:36 -0300

    Add missing controller

---
+++ CSharp/TfsCmdlets/Controllers/Admin/StartIdentitySyncController.cs
@@ -0,0 +1,14 @@
+using System.Text.RegularExpressions;
+using TfsCmdlets.Models;
+
+namespace TfsCmdlets.Controllers.Admin
+{
+    [CmdletController]
+    partial class StartIdentitySyncController
+    {
+        public override object InvokeCommand()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
