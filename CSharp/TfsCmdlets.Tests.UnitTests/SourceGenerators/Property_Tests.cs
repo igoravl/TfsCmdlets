6779326bafcb6aa05fe7c9b46f195107e2906e53
Igor Abade <igoravl@gmail.com>
2022-01-10 19:42:53 -0300

    Add unit test

---
+++ CSharp/TfsCmdlets.Tests.UnitTests/SourceGenerators/Property_Tests.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TfsCmdlets.SourceGenerators;
+using Xunit;
+
+namespace TfsCmdlets.Tests.UnitTests.SourceGenerators
+{
+    public class Property_Tests: IClassFixture<SourceGeneratorFixture>
+    {
+        public SourceGeneratorFixture Fixture { get; }
+
+        public Property_Tests(SourceGeneratorFixture fixture)
+        {
+            Fixture = fixture;
+        }
+
+        [Fact]
+        public void Can_Get_Property_Initializer()
+        {
+            const string code = @"
+public namespace SampleNamespace
+{
+  public class SampleClass
+  {
+    public string MyProp {get;set;} = ""*"";
+  }
+}
+";
+
+
+            var inputCompilation = Fixture.CreateCompilation(code);
+            var prop = inputCompilation.GetTypeByMetadataName("SampleNamespace.SampleClass").GetMembers().OfType<IPropertySymbol>().First();
+
+            var propInfo = new GeneratedProperty(prop, string.Empty);
+
+            Assert.Equal("\"*\"", propInfo.DefaultValue);
+
+        }
+    }
+}
