c205c37d19b3a9513e8e219d750cfe3f62b30618
Igor Abade <igoravl@gmail.com>
2022-01-11 16:23:23 -0300

    Add overload to LogError

--- CSharp/TfsCmdlets.SourceGenerators/Logger.cs
+++ CSharp/TfsCmdlets.SourceGenerators/Logger.cs
@@ -11,16 +11,25 @@ namespace TfsCmdlets.SourceGenerators
     {
         private static List<string> Logs { get; } = new List<string>();
 
+        [Conditional("DEBUG")]
         internal static void Log(string message)
         {
             var msg = $"[{DateTime.Now.ToString("HH:mm:ss.fff")}] {message}";
-            
+
             Logs.Add($"/* {msg} */");
             Debug.WriteLine($"[TfsCmdlets.Generator] {msg}");
         }
 
-        internal static void LogError(Exception ex) => Log($"[ERROR] {ex}");
+        [Conditional("DEBUG")]
+        internal static void LogError(Exception ex)
+        {
+            Log($"[ERROR] {ex}");
+        }
 
+        [Conditional("DEBUG")]
+        internal static void LogError(Exception ex, string msg) => Log($"[ERROR] {msg} ({ex})");
+
+        [Conditional("DEBUG")]
         internal static void FlushLogs(GeneratorExecutionContext context)
         {
             context.AddSource($"logs.g.cs", SourceText.From(string.Join("\n", Logs), Encoding.UTF8));
