683bd01ef0ffe81802d921a6fbca862b39f0a698
Igor Abade <igoravl@gmail.com>
2022-01-20 05:51:28 -0300

    Add *-TfsOrganization aliases

--- PS/_Private/Aliases.ps1
+++ PS/_Private/Aliases.ps1
@@ -1,1 +1,4 @@
 Set-Alias -Name 'ctfs' -Value 'Connect-TfsTeamProjectCollection'
+Set-Alias -Name 'Connect-TfsOrganization' -Value 'Connect-TfsTeamProjectCollection'
+Set-Alias -Name 'Disconnect-TfsOrganization' -Value 'Disconnect-TfsTeamProjectCollection'
+Set-Alias -Name 'Get-TfsOrganization' -Value 'Get-TfsTeamProjectCollection'
