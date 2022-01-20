26764d381529fa76e967fa5109e82baa3db7bab0
Igor Abade <igoravl@gmail.com>
2022-01-19 21:03:49 -0300

    Add Models generator

---
+++ CSharp/TfsCmdlets.SourceGenerators/Generators/Models/Generator.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace TfsCmdlets.SourceGenerators.Generators.Models
+{
+    // [Generator]
+    public class Generator : BaseGenerator<Filter, TypeProcessor>
+    {
+    }
+}
---
+++ CSharp/TfsCmdlets.SourceGenerators/Generators/Models/TypeProcessor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Linq;
+
+namespace TfsCmdlets.SourceGenerators.Generators.Models
+{
+    public class TypeProcessor : BaseTypeProcessor
+    {
+        public object InnerType { get; set; }
+
+        protected override void OnInitialize()
+        {
+            var node = Type.BaseType.TypeArguments.FirstOrDefault();
+
+            InnerType = node;
+
+        }
+
+        public override string GenerateCode()
+        {
+            return $@"namespace {Namespace}
+{{
+/*
+InnerType: {InnerType.GetType()}
+*/
+    public partial class {Type.Name}
+    {{
+        public static implicit operator {Type}({InnerType} obj) => new {Type}(obj);
+        public static implicit operator {InnerType}({Type} obj) => obj.InnerObject;
+    }}
+}}
+";
+        }
+    }
+}
