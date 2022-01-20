23ea775c3c9732ccc96ce2ebba5d23a98d6969dc
Igor Abade <igoravl@gmail.com>
2022-01-19 22:50:55 -0300

    Disable UTs

--- CSharp/TfsCmdlets.Tests.UnitTests/Controllers/ControllerFixtures.cs
+++ CSharp/TfsCmdlets.Tests.UnitTests/Controllers/ControllerFixtures.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using NSubstitute;
-using TfsCmdlets.Services;
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Text;
+// using NSubstitute;
+// using TfsCmdlets.Services;
 
-namespace TfsCmdlets.Tests.UnitTests.Controllers
-{
-    public class ControllerFixtures: IDisposable
-    {
-        public IPowerShellService EmptyPowerShell { get; } = Substitute.For<IPowerShellService>();
+// namespace TfsCmdlets.Tests.UnitTests.Controllers
+// {
+//     public class ControllerFixtures: IDisposable
+//     {
+//         public IPowerShellService EmptyPowerShell { get; } = Substitute.For<IPowerShellService>();
 
-        public void Dispose()
-        {
-        }
-    }
-}
+//         public void Dispose()
+//         {
+//         }
+//     }
+// }
--- CSharp/TfsCmdlets.Tests.UnitTests/Fixtures/SourceGeneratorFixture.cs
+++ CSharp/TfsCmdlets.Tests.UnitTests/Fixtures/SourceGeneratorFixture.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Text;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Reflection;
+// using System.Text;
+// using Microsoft.CodeAnalysis;
+// using Microsoft.CodeAnalysis.CSharp;
 
-namespace TfsCmdlets.Tests.UnitTests
-{
-    public class SourceGeneratorFixture: IDisposable
-    {
-        public Compilation CreateCompilation(string source)
-            => CSharpCompilation.Create("compilation",
-                new[] { CSharpSyntaxTree.ParseText(source) },
-                new[] { MetadataReference.CreateFromFile(typeof(Binder).GetTypeInfo().Assembly.Location) },
-                new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+// namespace TfsCmdlets.Tests.UnitTests
+// {
+//     public class SourceGeneratorFixture: IDisposable
+//     {
+//         public Compilation CreateCompilation(string source)
+//             => CSharpCompilation.Create("compilation",
+//                 new[] { CSharpSyntaxTree.ParseText(source) },
+//                 new[] { MetadataReference.CreateFromFile(typeof(Binder).GetTypeInfo().Assembly.Location) },
+//                 new CSharpCompilationOptions(OutputKind.ConsoleApplication));
 
-        public void Dispose()
-        {
-        }
-    }
-}
+//         public void Dispose()
+//         {
+//         }
+//     }
+// }
