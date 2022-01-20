a66431fd2f0cf3c6ad51fa6be12994648e2b49ea
Igor Abade <igoravl@gmail.com>
2022-01-20 11:55:13 -0300

    Fix GetProcessTemplate

--- CSharp/TfsCmdlets/Controllers/ProcessTemplate/GetProcessTemplateController.cs
+++ CSharp/TfsCmdlets/Controllers/ProcessTemplate/GetProcessTemplateController.cs
@@ -9,56 +9,58 @@ namespace TfsCmdlets.Controllers.ProcessTemplate
     [CmdletController(typeof(WebApiProcess))]
     partial class GetProcessTemplateController
     {
-       protected override IEnumerable Run()
-       {
-           var process = Parameters.Get<object>(nameof(GetProcessTemplate.ProcessTemplate));
-           var isDefault = Parameters.Get<bool>(nameof(GetProcessTemplate.Default));
+        protected override IEnumerable Run()
+        {
+            var client = Data.GetClient<ProcessHttpClient>();
 
-           var client = Data.GetClient<ProcessHttpClient>();
+            foreach (var pt in ProcessTemplate)
+            {
+                var process = pt switch
+                {
+                    string s0 when s0.IsGuid() => new Guid(s0),
+                    _ => pt
+                };
 
-           while (true) switch (process)
-               {
-                   case WebApiProcess p:
-                       {
-                           yield return p;
-                           yield break;
-                       }
-                   case string s when s.IsGuid():
-                       {
-                           process = new Guid(s);
-                           continue;
-                       }
-                   case Guid g:
-                       {
-                           yield return TaskExtensions.GetResult<Process>(client.GetProcessByIdAsync(g), $"Error getting process template '{process}'");
+                switch (process)
+                {
+                    case WebApiProcess p:
+                        {
+                            yield return p;
+                            break;
+                        }
+                    case Guid g:
+                        {
+                            yield return TaskExtensions.GetResult<Process>(client.GetProcessByIdAsync(g), $"Error getting process template '{process}'");
 
-                           yield break;
-                       }
-                   case object o when isDefault:
-                       {
-                           foreach (var proc in TaskExtensions.GetResult<List<Process>>(client.GetProcessesAsync(), $"Error getting process templates")
-                               .Where(p => p.IsDefault))
-                           {
-                               yield return proc;
-                           }
+                            yield break;
+                        }
+                    case null when Default:
+                    case { } when Default:
+                        {
+                            foreach (var proc in TaskExtensions.GetResult<List<Process>>(client.GetProcessesAsync(), $"Error getting process templates")
+                                .Where(p => p.IsDefault))
+                            {
+                                yield return proc;
+                            }
 
-                           yield break;
-                       }
-                   case string s:
-                       {
-                           foreach (var proc in TaskExtensions.GetResult<List<Process>>(client.GetProcessesAsync(), $"Error getting process template '{process}'")
-                               .Where(p => p.Name.IsLike(s)))
-                           {
-                               yield return proc;
-                           }
+                            yield break;
+                        }
+                    case string s:
+                        {
+                            foreach (var proc in TaskExtensions.GetResult<List<Process>>(client.GetProcessesAsync(), $"Error getting process template '{process}'")
+                                .Where(p => p.Name.IsLike(s)))
+                            {
+                                yield return proc;
+                            }
 
-                           yield break;
-                       }
-                   default:
-                       {
-                           throw new ArgumentException($"Invalid or non-existent process '{process}'");
-                       }
-               }
-       }
+                            yield break;
+                        }
+                    default:
+                        {
+                            throw new ArgumentException($"Invalid or non-existent process '{process}'");
+                        }
+                }
+            }
+        }
     }
 }
