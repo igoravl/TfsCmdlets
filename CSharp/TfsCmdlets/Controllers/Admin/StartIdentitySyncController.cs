8c6a81c09228510ec2493d3ae6fb24f17a37896f
Igor Abade <igoravl@gmail.com>
2022-01-24 16:20:36 -0300

    Move to legacy

--- CSharp/TfsCmdlets/Controllers/Admin/StartIdentitySyncController.cs
+++ CSharp/TfsCmdlets/Controllers/Admin/StartIdentitySyncController.cs
@@ -1,14 +1,11 @@
-using System.Text.RegularExpressions;
-using TfsCmdlets.Models;
-
-namespace TfsCmdlets.Controllers.Admin
-{
-    [CmdletController]
-    partial class StartIdentitySyncController
-    {
-        protected override IEnumerable Run()
-        {
-            throw new System.NotImplementedException();
-        }
-    }
-}
+// namespace TfsCmdlets.Controllers.Admin
+// {
+//     [CmdletController]
+//     partial class StartIdentitySyncController
+//     {
+//         protected override IEnumerable Run()
+//         {
+//             throw new NotImplementedException();
+//         }
+//     }
+// }
