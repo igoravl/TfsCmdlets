565209de40bbeb5a0f59c1a68b6a1675398729e4
Igor Abade <igoravl@gmail.com>
2022-04-07 17:56:20 -0300

    Fix pipeline handling

--- CSharp/TfsCmdlets/Controllers/TeamProject/Avatar/RemoveTeamProjectAvatarController.cs
+++ CSharp/TfsCmdlets/Controllers/TeamProject/Avatar/RemoveTeamProjectAvatarController.cs
@@ -7,17 +7,17 @@ namespace TfsCmdlets.Controllers.TeamProject.Avatar
     {
         protected override IEnumerable Run()
         {
-            var tp = Data.GetProject();
             var client = Data.GetClient<ProjectHttpClient>();
 
-            if (PowerShell.ShouldProcess(tp, $"Reset custom team project avatar image to default"))
+            foreach (var tp in Data.GetItems<WebApiTeamProject>(new { Project = Parameters.Get<object>(nameof(Project)) }))
             {
+                if (!PowerShell.ShouldProcess($"[Project: {tp.Name}]", $"Remove custom team project avatar")) continue;
+
                 Logger.Log($"Resetting team project avatar image to default");
 
                 client.RemoveProjectAvatarAsync(tp.Name)
                     .Wait("Error removing project avatar");
             }
-
             return null;
         }
     }
