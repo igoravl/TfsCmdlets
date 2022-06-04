0ad27cf0d0b7d57e96702129371afd2292c97db3
Igor Abade <igoravl@gmail.com>
2022-06-03 21:36:05 -0300

    Add GetVariable controller

--- CSharp/TfsCmdlets/Controllers/Pipeline/ReleaseManagement/GetReleaseDefinitionController.cs
+++ CSharp/TfsCmdlets/Controllers/Pipeline/ReleaseManagement/GetReleaseDefinitionController.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.Services.ReleaseManagement.WebApi;
 using Microsoft.VisualStudio.Services.ReleaseManagement.WebApi.Clients;
+using Microsoft.VisualStudio.Services.ReleaseManagement.WebApi.Contracts;
 
 namespace TfsCmdlets.Controllers.Pipeline.ReleaseManagement
 {
@@ -19,18 +20,22 @@ namespace TfsCmdlets.Controllers.Pipeline.ReleaseManagement
                     _ => input,
                 };
 
+                var expand = DoNotIncludeDetails? 
+                    ReleaseDefinitionExpands.LastRelease :
+                    ReleaseDefinitionExpands.Artifacts | ReleaseDefinitionExpands.Environments | ReleaseDefinitionExpands.Triggers | ReleaseDefinitionExpands.Variables | ReleaseDefinitionExpands.Tags | ReleaseDefinitionExpands.LastRelease;
+
                 switch (definition)
                 {
                     case ReleaseDefinition rd: 
                         yield return rd;
                         break;
                     case string s when s.IsWildcard():
-                        yield return client.GetReleaseDefinitionsAsync(Project.Name)
+                        yield return client.GetReleaseDefinitionsAsync(Project.Name, expand: expand)
                             .GetResult($"Error getting release definition(s) '{s}'")
                             .Where(r => r.Name.IsLike(s));
                         break;
                     case string s: 
-                        yield return client.GetReleaseDefinitionsAsync(Project.Name, searchText: s)
+                        yield return client.GetReleaseDefinitionsAsync(Project.Name, searchText: s, expand: expand)
                             .GetResult($"Error getting release definition '{s}'");
                         break;
                 }
---
+++ CSharp/TfsCmdlets/Controllers/Pipeline/Variable/GetVariableController.cs
@@ -0,0 +1,35 @@
+using Microsoft.TeamFoundation.DistributedTask.WebApi;
+using WebApiVariableGroup = Microsoft.TeamFoundation.DistributedTask.WebApi.VariableGroup;
+
+namespace TfsCmdlets.Controllers.Pipeline.Variable
+{
+    [CmdletController]
+    partial class GetVariableController
+    {
+        protected override IEnumerable Run()
+        {
+            return ParameterSetName switch
+            {
+                "By Pipeline" => GetByPipeline(),
+                "By Release" => GetByRelease(),
+                "By Variable Group" => GetByVariableGroup(),
+                _ => throw new ArgumentException($"Invalid ParameterSetName '{ParameterSetName}'"),
+            };
+        }
+
+        private IEnumerable GetByVariableGroup()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable GetByRelease()
+        {
+            var releaseDefinition = GetItem
+        }
+
+        private IEnumerable GetByPipeline()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
