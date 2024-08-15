41a16774eec3ba5be131836ceb94ca9edce79b11
Igor Abade <igoravl@gmail.com>
2024-08-15 00:53:03 -0300

    Rename fiile

--- PS/_Tests/WorkItem/Field/GetTfsWorkItemField.Tests.ps1
+++
@@ -1,8 +0,0 @@
-﻿& "$(($PSScriptRoot -split '_Tests')[0])/_Tests/_TestSetup.ps1"
-
-Describe (($MyInvocation.MyCommand.Name -split '\.')[-3]) {
-
-    Context 'Integration Tests' {
-        Write-Warning "$(Split-Path $PSCommandPath -Leaf): Test not implemented"
-    } 
-}
