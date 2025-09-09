32fd4b66693d5729f527e3e8105bd13b8560a86f
Igor Abade <igoravl@gmail.com>
2025-09-09 05:10:44 -0300

    Remove obsolete files

--- CSharp/TfsCmdlets.SourceGenerators/Generators/Models/Filter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.CodeAnalysis;
-
-namespace TfsCmdlets.SourceGenerators.Generators.Models
-{
-    public class Filter : BaseFilter
-    {
-        public override bool ShouldProcessType(INamedTypeSymbol type) => type.HasAttribute("ModelAttribute");
-    }
-}
