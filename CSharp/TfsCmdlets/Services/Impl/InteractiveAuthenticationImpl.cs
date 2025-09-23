3acbc9f306619099103a118dd50ebe376c658961
Igor Abade <igoravl@gmail.com>
2025-09-23 00:46:39 -0300

    Remove obsolete using directive

--- CSharp/TfsCmdlets/Services/Impl/InteractiveAuthenticationImpl.cs
+++ CSharp/TfsCmdlets/Services/Impl/InteractiveAuthenticationImpl.cs
@@ -1,5 +1,5 @@
 using Microsoft.Identity.Client;
-using Microsoft.Identity.Client.Desktop;
+// using Microsoft.Identity.Client.Desktop;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
