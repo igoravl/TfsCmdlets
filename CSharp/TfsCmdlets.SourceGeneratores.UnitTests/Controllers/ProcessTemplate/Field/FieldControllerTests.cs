bc0f13994da44d053564e306121424b2d95a2db6
Igor Abade <igoravl@gmail.com>
2025-09-24 03:52:49 -0300

    Disable tests for disabled cmdlets

--- CSharp/TfsCmdlets.SourceGeneratores.UnitTests/Controllers/ProcessTemplate/Field/FieldControllerTests.cs
+++ CSharp/TfsCmdlets.SourceGeneratores.UnitTests/Controllers/ProcessTemplate/Field/FieldControllerTests.cs
@@ -2,27 +2,27 @@ namespace TfsCmdlets.SourceGenerators.UnitTests.Controllers.ProcessTemplate.Fiel
 
 public partial class FieldControllerTests
 {
-    [Fact]
-    public async Task CanGenerate_GetProcessFieldDefinitionController()
-    {
-        await TestHelper.VerifyFiles<Generators.Controllers.ControllerGenerator>(
-            nameof(CanGenerate_GetProcessFieldDefinitionController),
-            new[]
-            {
-                "TfsCmdlets\\Cmdlets\\ProcessTemplate\\Field\\GetProcessFieldDefinition.cs"
-            });
-    }
+    //[Fact]
+    //public async Task CanGenerate_GetProcessFieldDefinitionController()
+    //{
+    //    await TestHelper.VerifyFiles<Generators.Controllers.ControllerGenerator>(
+    //        nameof(CanGenerate_GetProcessFieldDefinitionController),
+    //        new[]
+    //        {
+    //            "TfsCmdlets\\Cmdlets\\ProcessTemplate\\Field\\GetProcessFieldDefinition.cs"
+    //        });
+    //}
 
-    [Fact]
-    public async Task CanGenerate_NewProcessFieldDefinitionController()
-    {
-        await TestHelper.VerifyFiles<Generators.Controllers.ControllerGenerator>(
-            nameof(CanGenerate_NewProcessFieldDefinitionController),
-            new[]
-            {
-                "TfsCmdlets\\Cmdlets\\ProcessTemplate\\Field\\NewProcessFieldDefinition.cs"
-            });
-    }
+    //[Fact]
+    //public async Task CanGenerate_NewProcessFieldDefinitionController()
+    //{
+    //    await TestHelper.VerifyFiles<Generators.Controllers.ControllerGenerator>(
+    //        nameof(CanGenerate_NewProcessFieldDefinitionController),
+    //        new[]
+    //        {
+    //            "TfsCmdlets\\Cmdlets\\ProcessTemplate\\Field\\NewProcessFieldDefinition.cs"
+    //        });
+    //}
 
     [Fact]
     public async Task CanGenerate_RemoveProcessFieldDefinitionController()
